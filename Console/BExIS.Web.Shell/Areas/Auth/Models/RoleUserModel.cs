e19befb8a18aa9497cb9c770c5e1f02e896d463f
Sven Thiel <BEXIS2\Sven Thiel>
2013-09-04 19:52:43 +0000

    model updates
    work-items: #77
    git-tfs-id: [http://bexis2.vmguest.uni-jena.de:8080/tfs]$/Tech/Dev;C270

--- Console/BExIS.Web.Shell/Areas/Auth/Models/RoleUserModel.cs
+++ Console/BExIS.Web.Shell/Areas/Auth/Models/RoleUserModel.cs
@@ -8,16 +8,20 @@ namespace BExIS.Web.Shell.Areas.Auth.Models
 {
     public class RoleUserModel
     {
+        public long RoleId { get; set; }
+
         public long Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
 
         public bool UserInRole { get; set; }
 
-        public static RoleUserModel Convert(User user, bool userInRole)
+        public static RoleUserModel Convert(long roleId, User user, bool userInRole)
         {
             return new RoleUserModel()
             {
+                RoleId = user.Id,
+
                 Id = user.Id,
                 UserName = user.Name,
                 Email = user.Email,
--- Console/BExIS.Web.Shell/Areas/Auth/Models/UserRoleModel.cs
+++ Console/BExIS.Web.Shell/Areas/Auth/Models/UserRoleModel.cs
@@ -8,16 +8,23 @@ namespace BExIS.Web.Shell.Areas.Auth.Models
 {
     public class UserRoleModel
     {
+        // User
+        public long UserId { get; set; }
+
+        // Role
         public long Id { get; set; }
         public string RoleName { get; set; }
         public string Description { get; set; }
 
+        // Membership
         public bool UserInRole { get; set; }
 
-        public static UserRoleModel Convert(Role role, bool userInRole)
+        public static UserRoleModel Convert(long userId, Role role, bool userInRole)
         {
             return new UserRoleModel()
             {
+                UserId = userId,
+
                 Id = role.Id,
                 RoleName = role.Name,
                 Description = role.Description,
