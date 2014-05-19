d412c97a320d44e8e6a79202360384861a091f28
Javad Chamanara <BEXIS2\Javad Chamanara>
2014-05-19 14:02:14 +0000

    RI deom C_DLM
    work-items: #418
    git-tfs-id: [http://bexis2.vmguest.uni-jena.de:8080/tfs]$/Tech/Dev;C670

---
+++ Console/BExIS.Web.Shell/Areas/SAM/Controllers/UnsafeController.cs
@@ -0,0 +1,36 @@
+﻿using BExIS.Dlm.Services.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Web.Shell.Areas.SAM.Controllers
+{
+    public class UnsafeController : Controller
+    {
+        //
+        // GET: /SAM/Unsafe/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PurgeDataset(long id)
+        {
+            DatasetManager dm = new DatasetManager();
+            bool b = dm.PurgeDataset(id);
+            return View();
+        }
+
+        public ActionResult ListDatasets()
+        {
+            DatasetManager dm = new DatasetManager();
+            List<Int64> ids = dm.DatasetRepo.Query().Select(p => p.Id).ToList();
+            return View(ids);
+        }
+
+
+    }
+}
---
+++ Console/BExIS.Web.Shell/Areas/SAM/SAMAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace BExIS.Web.Shell.Areas.SAM
+{
+    public class SAMAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "SAM";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "SAM_default",
+                "SAM/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
