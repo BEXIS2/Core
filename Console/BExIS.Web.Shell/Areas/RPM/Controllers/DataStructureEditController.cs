c70fa04ad764c68bcdcd330c867621ecef073395
Martin Hohmuth <BEXIS2\Martin Hohmuth>
2016-02-01 13:56:51 +0000

    Merge for git
    work-items: #5377
    git-tfs-id: [http://bexis2.vmguest.uni-jena.de:8080/tfs]$/Tech/Dev;C2192

---
+++ Console/BExIS.Web.Shell/Areas/RPM/Controllers/DataStructureEditController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.Mvc;
+using Telerik.Web.Mvc;
+
+using BExIS.Web.Shell.Areas.RPM.Models;
+
+namespace BExIS.Web.Shell.Areas.RPM.Controllers
+{
+    public class DataStructureEditController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+
+}
---
+++ Console/BExIS.Web.Shell/Areas/RPM/Models/DataStructureEditModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using BExIS.Dlm.Entities.DataStructure;
+using BExIS.Dlm.Services.DataStructure;
+
+namespace BExIS.Web.Shell.Areas.RPM.Models
+{
+    public class AttributePreviewStruct
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Unit { get; set; }
+        public string DataType { get; set; }
+
+        public AttributePreviewStruct()
+        {
+            this.Id = 0;
+            this.Name = "";
+            this.Description = "";
+            this.Unit = "";
+            this.DataType = "";
+        }
+    }
+}
+    
