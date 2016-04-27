03a8812719c499b83359676ece0b00ac15fbfbdd
Sven Thiel <sventhiel@DESKTOP-K3DLEE5>
2016-04-27 12:49:04 +0200

    added footer

---
+++ Console/BExIS.Web.Shell/Areas/Site/Models/ContactUsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Web.Shell.Areas.Site.Models
+{
+    public class ContactUsModel
+    {
+        public string content { get; set; }
+
+        public ContactUsModel()
+        {
+            content = "";
+        }
+    }
+}
---
+++ Console/BExIS.Web.Shell/Areas/Site/Models/ShowFooterModel.cs
@@ -0,0 +1,19 @@
+﻿using BExIS.IO.Transform.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Web.Shell.Areas.Site.Models
+{
+    public class ShowFooterModel
+    {
+        public string Description { get; set; }
+
+        public ShowFooterModel()
+        {
+            Description = ".";
+        }
+    }
+}
