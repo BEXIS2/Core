3426516537eb3704ab4d5a1b7be43d19670916f6
Javad Chamanara <javad.chamanara@uni-jena.de>
2016-05-31 13:55:21 +0200

    Added Web API to retrieve primary data.

---
+++ Console/BExIS.Web.Shell/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using BExIS.Web.Shell.Areas.DIM.Models.Formatters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BExIS.Web.Shell
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+            ConfigureApis(config);
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+
+        public static void ConfigureApis(HttpConfiguration config)
+        {
+            config.Formatters.Add(new DataTupleCsvFormatter());
+        }
+    }
+}
