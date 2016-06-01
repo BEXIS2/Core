355a6fc4f9133bea6a24de9395737afe0d0c1af8
Sven Thiel <sven.thiel@uni-jena.de>
2016-06-01 11:16:05 +0200

    created views and models for tenant management
    Verknüpfte Arbeitselemente: #1253

---
+++ Console/BExIS.Web.Shell/Areas/SAM/Models/TenantModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Web.Shell.Areas.SAM.Models
+{
+    public class TenantCreateModel
+    {
+
+    }
+
+    public class TenantViewModel
+    {
+        
+    }
+
+    public class TenantGridRowModel
+    {
+        
+    }
+}
