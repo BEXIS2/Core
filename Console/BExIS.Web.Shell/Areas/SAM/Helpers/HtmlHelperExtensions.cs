113f26b230ce6fc37ee48d4c3e1e46a9f497d60c
Sven Thiel <sven.thiel@uni-jena.de>
2016-06-02 16:07:20 +0200

    first changes to support multi tenants (e.g. replacing images and so on), added HTML helper to allow image rendering from outside of the application environment
    Verknüpfte Arbeitselemente: #1254

---
+++ Console/BExIS.Web.Shell/Areas/SAM/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Web.Shell.Areas.SAM.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString Brand(this HtmlHelper htmlHelper, string path, string name)
+        {
+            byte[] image = File.ReadAllBytes(path);
+            string mime = MimeMapping.GetMimeMapping(path);
+            
+
+            return new MvcHtmlString(string.Format("<image src='data:{0};charset=utf-8;base64, {1}' alt='{2}' style='height: 40px; margin-top: -10px;' />", mime, Convert.ToBase64String(image), name));
+        }
+    }
+}
