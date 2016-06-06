0eefca46d784da7c2fdf206521579019ce391b73
Sven Thiel <sven.thiel@uni-jena.de>
2016-06-06 13:30:31 +0200

    modified the tenant views

--- Console/BExIS.Web.Shell/Areas/Site/Controllers/FooterController.cs
+++ Console/BExIS.Web.Shell/Areas/Site/Controllers/FooterController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vaiona.Web.Extensions;
+using Vaiona.Web.Mvc.Models;
 
 namespace BExIS.Web.Shell.Areas.Site.Controllers
 {
@@ -17,17 +18,19 @@ namespace BExIS.Web.Shell.Areas.Site.Controllers
 
         public ActionResult ContactUs()
         {
-
+            ViewBag.Title = PresentationModel.GetViewTitle("Contact Us");
             return View("Content", Session.GetTenant().ContactUsFileNamePath);
         }
 
         public ActionResult Imprint()
         {
+            ViewBag.Title = PresentationModel.GetViewTitle("Imprint");
             return View("Content", Session.GetTenant().ImprintFileNamePath);
         }
 
         public ActionResult Policy()
         {
+            ViewBag.Title = PresentationModel.GetViewTitle("Policy");
             return View("Content", Session.GetTenant().PolicyFileNamePath);
         }
     }
