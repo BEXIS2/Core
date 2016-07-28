40bbfa19942f4ca90d950188897fbede7e110ba7
David Blaa <dblaa@bgc-jena.mpg.de>
2016-07-28 15:32:07 +0200

    update text
    Related Work Items: #1272

--- Components/XML/BExIS.Xml.Helpers/Mapping/XmlMapperManager.cs
+++ Components/XML/BExIS.Xml.Helpers/Mapping/XmlMapperManager.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Xml;
-using System.Xml.Linq;
 using System.Xml.Schema;
-using BExIS.Xml.Models;
 using BExIS.Xml.Models.Mapping;
 using Vaiona.Utils.Cfg;
 
