39cf3f57bd3f57a662449dcc581b07b48f5fff0a
David Blaa <dblaa@bgc-jena.mpg.de>
2016-11-17 11:13:36 +0100

    Register Form - error messages disapear after clicking reset

--- Console/BExIS.Web.Shell/Areas/SAM/Models/AccountModels.cs
+++ Console/BExIS.Web.Shell/Areas/SAM/Models/AccountModels.cs
@@ -9,42 +9,42 @@ namespace BExIS.Web.Shell.Areas.SAM.Models
     public class AccountRegisterModel
     {
         [Display(Name = "Username")]
-        [RegularExpression("^[\\S]*$", ErrorMessage = "The Username must be without spaces.")]
+        [RegularExpression("^[\\S]*$", ErrorMessage = "Username must be without spaces.")]
         [Remote("ValidateUsername", "Account")]
-        [Required(AllowEmptyStrings = false)]
-        [StringLength(64, ErrorMessage = "The Username must be {2} - {1} characters long.", MinimumLength = 3)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Username is required.")]
+        [StringLength(64, ErrorMessage = "Username must be {2} - {1} characters long.", MinimumLength = 3)]
         public string Username { get; set; }
 
         [Display(Name = "Password")]
         [RegularExpression("^[\\S]*$", ErrorMessage = "The Password must not contain spaces.")]
-        [Required]
-        [StringLength(24, ErrorMessage = "The Password must be {2} - {1} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(24, ErrorMessage = "Password must be {2} - {1} characters long.", MinimumLength = 6)]
         public string Password { get; set; }
 
-        [global::System.Web.Mvc.Compare("Password", ErrorMessage = "The Password and Confirm Password do not match.")]
+        [global::System.Web.Mvc.Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
         [Display(Name = "Confirm Password")]
-        [Required]
+        [Required(ErrorMessage = "Confirm Password is required.")]
         public string ConfirmPassword { get; set; }
 
         [Display(Name = "Full Name")]
-        [Required]
+        [Required(ErrorMessage = "Full Name is required.")]
         public string FullName { get; set; }
 
         [Display(Name = "Email Address")]
         [Email]
         [Remote("ValidateEmail", "Account")]
-        [Required]
-        [StringLength(250, ErrorMessage = "The Email Address must be {2} - {1} characters long.", MinimumLength = 5)]
+        [Required(ErrorMessage = "Email Address is required.")]
+        [StringLength(250, ErrorMessage = "Email Address must be {2} - {1} characters long.", MinimumLength = 5)]
         public string Email { get; set; }
 
         [Display(Name = "Security Question")]
-        [Required]
+        [Required(ErrorMessage = "Security Question is required.")]
         public long SecurityQuestion { get; set; }
 
         [Display(Name = "Security Answer")]
-        [RegularExpression("^[^\\s]+(\\s+[^\\s]+)*", ErrorMessage = "The Security Answer must start and end with no space.")]
-        [Required]
-        [StringLength(50, ErrorMessage = "The Security Answer must be less than {1} characters long.")]
+        [RegularExpression("^[^\\s]+(\\s+[^\\s]+)*", ErrorMessage = "Security Answer must start and end with no space.")]
+        [Required(ErrorMessage = "Security Answer is required.")]
+        [StringLength(50, ErrorMessage = "Security Answer must be less than {1} characters long.")]
         public string SecurityAnswer { get; set; }
 
         public SecurityQuestionSelectListModel SecurityQuestionList { get; set; }
