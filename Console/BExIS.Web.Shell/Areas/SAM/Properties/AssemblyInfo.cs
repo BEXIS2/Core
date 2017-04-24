45249edbb390253fc7138b609b63d2e25a98ae21
sventhiel <sven.thiel@uni-jena.de>
2017-04-24 15:57:35 +0200

    update project properties

--- Console/BExIS.Web.Shell/Areas/SAM/Properties/AssemblyInfo.cs
+++ Console/BExIS.Web.Shell/Areas/SAM/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BAS.UI")]
+[assembly: AssemblyTitle("BExIS.Modules.Sam.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BAS.UI")]
+[assembly: AssemblyProduct("BExIS.Modules.Sam.UI")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
