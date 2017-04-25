8d6079e1ffb00fae0fbd8ab6152f5f31eb329c96
sventhiel <sven.thiel@uni-jena.de>
2017-04-25 10:06:12 +0200

    update permission entities

--- Components/AAA/BExIS.Security.Entities/Authorization/EntityPermission.cs
+++ Components/AAA/BExIS.Security.Entities/Authorization/EntityPermission.cs
@@ -2,9 +2,18 @@
 
 namespace BExIS.Security.Entities.Authorization
 {
+    public enum RightType
+    {
+        Read = 0,
+        Write = 1,
+        Delete = 2,
+        Grant = 3
+    }
+
     public class EntityPermission : Permission
     {
         public virtual Entity Entity { get; set; }
         public virtual long Key { get; set; }
+        public virtual short Rights { get; set; }
     }
 }
--- Components/AAA/BExIS.Security.Entities/Authorization/Permission.cs
+++ Components/AAA/BExIS.Security.Entities/Authorization/Permission.cs
@@ -9,18 +9,9 @@ namespace BExIS.Security.Entities.Authorization
         Grant = 1
     }
 
-    public enum RightType
-    {
-        Read = 0,
-        Write = 1,
-        Delete = 2,
-        Grant = 3
-    }
-
     public abstract class Permission : BaseEntity
     {
         public virtual PermissionType PermissionType { get; set; }
-        public virtual short Rights { get; set; }
         public virtual Subject Subject { get; set; }
     }
 }
