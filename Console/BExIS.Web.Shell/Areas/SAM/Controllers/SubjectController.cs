c057e222600c4205873400f9ae22f3c65795b991
Sven Thiel <sven.thiel@uni-jena.de>
2017-04-25 11:19:58 +0200

    combining group and user controller into one common subject controller

--- Console/BExIS.Web.Shell/Areas/SAM/Controllers/SubjectController.cs
+++ Console/BExIS.Web.Shell/Areas/SAM/Controllers/SubjectController.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using BExIS.Modules.Sam.UI.Models;
+using System;
 using System.Web.Mvc;
+using Telerik.Web.Mvc;
 
 namespace BExIS.Modules.Sam.UI.Controllers
 {
     public class SubjectController : Controller
     {
-        // GET: Subject
+        public ActionResult CreateGroup()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult CreateGroup(CreateGroupModel model)
+        {
+            return View(model);
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult CreateUser(CreateUserModel model)
+        {
+            return View(model);
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [GridAction]
+        public ActionResult Subjects_Select()
+        {
+            throw new NotImplementedException();
         }
     }
 }
