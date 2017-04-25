11b57561604ee9cf9008bb5eb93786bc1494cd9f
Sven Thiel <sven.thiel@uni-jena.de>
2017-04-25 11:21:01 +0200

    remove old model class in respect to new subject controller

---
+++ Console/BExIS.Web.Shell/Areas/SAM/Models/SubjectModels.cs
@@ -0,0 +1,21 @@
+﻿namespace BExIS.Modules.Sam.UI.Models
+{
+    public class CreateGroupModel
+    {
+        public string GroupName { get; set; }
+    }
+
+    public class CreateUserModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class UpdateGroupModel
+    {
+    }
+
+    public class UpdateUserModel
+    {
+    }
+}
