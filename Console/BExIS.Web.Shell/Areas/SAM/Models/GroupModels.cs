4d590435134ffd1bed317527d9ef101b405e6981
Sven Thiel <sven.thiel@uni-jena.de>
2017-04-26 12:47:05 +0200

    replace subject models by more specific model classes for groups and users

---
+++ Console/BExIS.Web.Shell/Areas/SAM/Models/GroupModels.cs
@@ -0,0 +1,21 @@
+﻿using BExIS.Security.Entities.Subjects;
+
+namespace BExIS.Modules.Sam.UI.Models
+{
+    public class GroupGridRowModel
+    {
+        public string Description { get; set; }
+        public string GroupName { get; set; }
+        public long Id { get; set; }
+
+        public static GroupGridRowModel Convert(Group group)
+        {
+            return new GroupGridRowModel()
+            {
+                Description = group.Description,
+                GroupName = group.Name,
+                Id = group.Id
+            };
+        }
+    }
+}
---
+++ Console/BExIS.Web.Shell/Areas/SAM/Models/UserModels.cs
@@ -0,0 +1,14 @@
+﻿using BExIS.Security.Entities.Subjects;
+
+namespace BExIS.Modules.Sam.UI.Models
+{
+    public class UserGridRowModel
+    {
+        public static UserGridRowModel Convert(User user)
+        {
+            return new UserGridRowModel()
+            {
+            };
+        }
+    }
+}
