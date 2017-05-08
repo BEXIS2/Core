87b296c8279364a76ec4005d5dcab5111103d344
Sven Thiel <sven.thiel@uni-jena.de>
2017-05-08 14:57:25 +0200

    update logic for feature permissions

--- Console/BExIS.Web.Shell/Areas/SAM/Controllers/FeaturePermissionController.cs
+++ Console/BExIS.Web.Shell/Areas/SAM/Controllers/FeaturePermissionController.cs
@@ -1,9 +1,10 @@
 ﻿using BExIS.Modules.Sam.UI.Models;
-using BExIS.Security.Services.Authorization;
+using BExIS.Security.Services.Objects;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using Telerik.Web.Mvc;
+using Vaiona.Web.Extensions;
+using Vaiona.Web.Mvc.Models;
 
 namespace BExIS.Modules.Sam.UI.Controllers
 {
@@ -11,16 +12,26 @@ namespace BExIS.Modules.Sam.UI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Title = PresentationModel.GetViewTitleForTenant("Manage Features", this.Session.GetTenant());
+
+            var featureManager = new FeatureManager();
+
+            var features = new List<FeatureTreeViewItemModel>();
+
+            var roots = featureManager.FindRoots();
+            roots.ToList().ForEach(f => features.Add(FeatureTreeViewItemModel.Convert(f)));
+
+            return View(features.AsEnumerable());
         }
 
-        [GridAction]
-        public ActionResult Subjects_Select(GridActionAttribute filters)
+        public void SetFeaturePublicity(long featureId, bool value)
         {
-            var featurePermissionManager = new FeaturePermissionManager();
-            var featurePermissions = featurePermissionManager.FeaturePermissions.Select(f => FeaturePermissionGridRowModel.Convert(f)).ToList();
+            var featureManager = new FeatureManager();
 
-            return View(new GridModel<FeaturePermissionGridRowModel> { Data = featurePermissions });
+            var feature = featureManager.FindById(featureId);
+            feature.IsPublic = value;
+
+            featureManager.Update(feature);
         }
     }
 }
--- Console/BExIS.Web.Shell/Areas/SAM/Models/FeaturePermissionModels.cs
+++ Console/BExIS.Web.Shell/Areas/SAM/Models/FeaturePermissionModels.cs
@@ -1,13 +1,28 @@
-﻿using BExIS.Security.Entities.Authorization;
+﻿using BExIS.Security.Entities.Objects;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BExIS.Modules.Sam.UI.Models
 {
-    public class FeaturePermissionGridRowModel
+    public class FeatureTreeViewItemModel
     {
-        public static FeaturePermissionGridRowModel Convert(FeaturePermission featurePermission)
+        public List<FeatureTreeViewItemModel> Children { get; set; }
+        public string Description { get; set; }
+        public string FeatureName { get; set; }
+        public long Id { get; set; }
+        public bool IsPublic { get; set; }
+
+        public static FeatureTreeViewItemModel Convert(Feature feature)
         {
-            return new FeaturePermissionGridRowModel()
+            return new FeatureTreeViewItemModel()
             {
+                Id = feature.Id,
+                FeatureName = feature.Name,
+                Description = feature.Description,
+
+                IsPublic = feature.IsPublic,
+
+                Children = feature.Children.Select(Convert).ToList()
             };
         }
     }
