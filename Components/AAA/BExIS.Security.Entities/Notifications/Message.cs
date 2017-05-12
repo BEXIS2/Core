57a665326a11d900ad3a1cdfe5befccc22eee346
Sven Thiel <sven.thiel@uni-jena.de>
2017-05-12 10:47:51 +0200

    remove unnecessary references

--- Components/AAA/BExIS.Security.Entities/Notifications/Message.cs
+++ Components/AAA/BExIS.Security.Entities/Notifications/Message.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BExIS.Security.Entities.Notifications
+﻿namespace BExIS.Security.Entities.Notifications
 {
     public class Message
     {
