6bcf95c88d25fbede5160f58315780aa82283bc9
Sven Thiel <sven.thiel@uni-jena.de>
2017-05-22 16:03:07 +0200

    add cotroller to maintain entities

---
+++ Console/BExIS.Web.Shell/Areas/SAM/Controllers/EntityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.Sam.UI.Controllers
+{
+    public class EntityController : Controller
+    {
+        // GET: Entity
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
