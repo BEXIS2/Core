730d8bbf152d3a3edb00fc515aa01d6adbb4c732
Martin Hohmuth <martin.hohmuth@uni-jena.de>
2017-06-20 13:14:13 +0200

    Created RPM Module

---
+++ Console/BExIS.Web.Shell/Areas/RPM/RPMModule.cs
@@ -0,0 +1,33 @@
+﻿using Vaiona.Web.Mvc.Modularity;
+
+namespace BExIS.Modules.Rpm.UI
+{
+    public class RPMModule : ModuleBase
+    {
+        public RPMModule(): base("RPM")
+        {
+        }
+
+        /// <summary>
+        /// Registers current area with the routing engine.
+        /// The default route is automatically registred. Using the AreaName as route prefix and url sapce.
+        /// </summary>
+        /// <remarks>
+        /// <list type="number">
+        ///     <item>If you are happy with the defaul route, either leave the method as is or comment it all (prefered).</item>
+        ///     <item>if you want to register other than the default, comment the call to the base method and write your own ones.</item>
+        ///     <item>If you want to register additional routes, write them after the call to the base method.</item>
+        /// </list>
+        /// </remarks>
+        /// <param name="context"></param>
+        //public override void RegisterArea(AreaRegistrationContext context)
+        //{
+        //    base.RegisterArea(context);
+        //    //context.MapRoute(
+        //    //    AreaName + "_default",
+        //    //    AreaName+"/{controller}/{action}/{id}",
+        //    //    new { action = "Index", id = UrlParameter.Optional }
+        //    //);
+        //}
+    }
+}
