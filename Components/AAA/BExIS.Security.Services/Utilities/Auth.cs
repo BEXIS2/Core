5af07721021070b4fd4b5838bf129a3ce23568d2
Sven Thiel <sven.thiel@uni-jena.de>
2017-06-28 11:16:14 +0200

    update entities and services

--- Components/AAA/BExIS.Security.Services/Utilities/Auth.cs
+++ Components/AAA/BExIS.Security.Services/Utilities/Auth.cs
@@ -3,7 +3,6 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.DataProtection;
 using Microsoft.Owin.Security.Google;
-using Oakenshield.Security.Providers.LDAP;
 using Owin;
 
 namespace BExIS.Security.Services.Utilities
@@ -16,29 +15,28 @@ namespace BExIS.Security.Services.Utilities
         public static void Configure(IAppBuilder app)
         {
             DataProtectionProvider = app.GetDataProtectionProvider();
-            // Enable the application to use a cookie to store information for the signed in user
+
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/SignIn")
+                LoginPath = new PathString("/Account/Login"),
             });
-            // Use a cookie to temporarily store information about a user logging in with a third party login provider
-            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
             //    clientId: "",
             //    clientSecret: "");
 
-            app.UseTwitterAuthentication(
-               consumerKey: "",
-               consumerSecret: "");
+            //app.UseTwitterAuthentication(
+            //   consumerKey: "dfgdfgdfgd",
+            //   consumerSecret: "dfgdfffffffg");
 
-            app.UseLdapAuthentication(new LdapAuthenticationOptions("LDAP"));
+            //app.UseLdapAuthentication(new LdapAuthenticationOptions("/signin-ldap", "host"));
 
-            app.UseFacebookAuthentication(
-               appId: "",
-               appSecret: "");
+            //app.UseFacebookAuthentication(
+            //   appId: "sfg",
+            //   appSecret: "ffffffffffff");
 
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
