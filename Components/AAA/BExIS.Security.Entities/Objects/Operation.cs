4ba0f0bfd0c39e290ad02c7e11f1968ce41fee45
Sven Thiel <sven.thiel@uni-jena.de>
2017-06-30 14:39:02 +0200

    add nhibernate mapping file for operation

--- Components/AAA/BExIS.Security.Entities/Objects/Operation.cs
+++ Components/AAA/BExIS.Security.Entities/Objects/Operation.cs
@@ -3,6 +3,7 @@
     public class Operation
     {
         public virtual int Position { get; set; }
+        public virtual string Module { get; set; }
         public virtual string Controller { get; set; }
         public virtual string Action { get; set; }
     }
