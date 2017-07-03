192b5410a6ec296016ea040d585d54de4779ca33
Sven Thiel <sven.thiel@uni-jena.de>
2017-07-03 13:41:22 +0200

    update operation class

--- Components/AAA/BExIS.Security.Entities/Objects/Operation.cs
+++ Components/AAA/BExIS.Security.Entities/Objects/Operation.cs
@@ -1,10 +1,13 @@
-﻿namespace BExIS.Security.Entities.Objects
+﻿using Vaiona.Entities.Common;
+
+namespace BExIS.Security.Entities.Objects
 {
-    public class Operation
+    public class Operation : BaseEntity
     {
         public virtual int Position { get; set; }
         public virtual string Module { get; set; }
         public virtual string Controller { get; set; }
         public virtual string Action { get; set; }
+        public virtual Workflow Workflow { get; set; }
     }
 }
