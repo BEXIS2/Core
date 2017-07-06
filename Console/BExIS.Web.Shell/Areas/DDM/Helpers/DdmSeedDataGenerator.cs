370c581f46be1de72d1f6e3fd1f933b512d36d2c
David Blaa <dblaa@bgc-jena.mpg.de>
2017-07-06 09:51:58 +0200

    refactor seed data for ddm module
    Related Work Items: #1499

--- Console/BExIS.Web.Shell/Areas/DDM/Helpers/DdmSeedDataGenerator.cs
+++ Console/BExIS.Web.Shell/Areas/DDM/Helpers/DdmSeedDataGenerator.cs
@@ -1,10 +1,79 @@
 ﻿
+using BExIS.Security.Entities.Objects;
+using BExIS.Security.Services.Objects;
+
 namespace BExIS.Modules.Ddm.UI.Helpers
 {
     public class DdmSeedDataGenerator
     {
         public static void GenerateSeedData()
         {
+            #region SECURITY
+            //workflows = größere sachen, vielen operation
+            //operations = einzelne actions
+
+            //1.controller -> 1.Operation
+
+
+            FeatureManager featureManager = new FeatureManager();
+
+            Feature DataDiscovery = featureManager.Create("Search", "Search");
+
+
+            Feature SearchFeature = featureManager.Create("Search", "Search", DataDiscovery);
+            Feature SearchManagementFeature = featureManager.Create("Search Management", "Search Management", DataDiscovery);
+
+            //worklfows -> create dataset ->
+            WorkflowManager workflowManager = new WorkflowManager();
+
+            var operation = new Operation();
+            Workflow workflow = new Workflow();
+            OperationManager operationManager = new OperationManager();
+
+
+            #region Help Workflow
+
+            workflow = new Workflow();
+            workflow.Name = "Search Help";
+            workflowManager.Create(workflow);
+
+            operation = operationManager.Create("DDM", "Help", "*", null, workflow);
+            workflow.Operations.Add(operation);
+
+            DataDiscovery.Workflows.Add(workflow);
+
+            #endregion
+
+            #region Search Workflow
+
+            workflow = new Workflow();
+            workflow.Name = "Search";
+            workflowManager.Create(workflow);
+
+            operation = operationManager.Create("DDM", "Home", "*", null, workflow);
+            workflow.Operations.Add(operation);
+            operation = operationManager.Create("DDM", "Data", "*", null, workflow);
+            workflow.Operations.Add(operation);
+            SearchFeature.Workflows.Add(workflow);
+
+            #endregion
+
+            #region Search Admin Workflow
+
+            workflow = new Workflow();
+            workflow.Name = "Search Managment";
+            workflowManager.Create(workflow);
+
+            operation = operationManager.Create("DDM", "Admin", "*", null, workflow);
+            workflow.Operations.Add(operation);
+
+            SearchManagementFeature.Workflows.Add(workflow);
+
+            #endregion
+
+
+            #endregion
+
         }
     }
 }
