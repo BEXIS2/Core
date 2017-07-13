f2d06344af520893360210b8a71983df9e32c568
Sven Thiel <sven.thiel@uni-jena.de>
2017-07-13 15:14:00 +0200

    update permissions entities

--- Components/AAA/BExIS.Security.Entities/Authorization/FeaturePermission.cs
+++ Components/AAA/BExIS.Security.Entities/Authorization/FeaturePermission.cs
@@ -1,9 +1,13 @@
 ﻿using BExIS.Security.Entities.Objects;
+using BExIS.Security.Entities.Subjects;
+using Vaiona.Entities.Common;
 
 namespace BExIS.Security.Entities.Authorization
 {
-    public class FeaturePermission : Permission
+    public class FeaturePermission : BaseEntity
     {
         public virtual Feature Feature { get; set; }
+        public virtual PermissionType PermissionType { get; set; }
+        public virtual Subject Subject { get; set; }
     }
 }
---
+++ Components/AAA/BExIS.Security.Entities/Authorization/PermissionType.cs
@@ -0,0 +1,8 @@
+﻿namespace BExIS.Security.Entities.Authorization
+{
+    public enum PermissionType
+    {
+        Deny = 0,
+        Grant = 1
+    }
+}
