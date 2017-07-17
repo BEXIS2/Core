dc7a40db1cf156776a369ae34f999402a1f7eb9e
Sven Thiel <sven.thiel@uni-jena.de>
2017-07-17 15:08:20 +0200

    update methods to check null

--- Components/AAA/BExIS.Security.Services/Authorization/EntityPermissionManager.cs
+++ Components/AAA/BExIS.Security.Services/Authorization/EntityPermissionManager.cs
@@ -55,6 +55,15 @@ namespace BExIS.Security.Services.Authorization
 
         public EntityPermission Create<T>(string subjectName, string entityName, Type entityType, long key, List<RightType> rights) where T : Subject
         {
+            if (string.IsNullOrEmpty(subjectName))
+                return null;
+
+            if (string.IsNullOrEmpty(entityName))
+                return null;
+
+            if (entityType == null)
+                return null;
+
             var entityPermission = new EntityPermission()
             {
                 Subject = SubjectRepository.Query(s => s.Name.ToUpperInvariant() == subjectName.ToUpperInvariant() && s is T).FirstOrDefault(),
@@ -112,8 +121,22 @@ namespace BExIS.Security.Services.Authorization
 
         public List<long> GetKeys<T>(string subjectName, string entityName, Type entityType, RightType rightType) where T : Subject
         {
+            if (string.IsNullOrEmpty(subjectName))
+                return new List<long>();
+
+            if (string.IsNullOrEmpty(entityName))
+                return new List<long>();
+
+            if (entityType == null)
+                return new List<long>();
+
             var subject = SubjectRepository.Query(s => s.Name.ToUpperInvariant() == subjectName.ToUpperInvariant() && s is T).FirstOrDefault();
+            if (subject == null)
+                return new List<long>();
+
             var entity = EntityRepository.Query(e => e.Name.ToUpperInvariant() == entityName.ToUpperInvariant() && e.EntityType == entityType).FirstOrDefault();
+            if (entity == null)
+                return new List<long>();
 
             return
                 EntityPermissionRepository.Get().Where(
@@ -125,7 +148,12 @@ namespace BExIS.Security.Services.Authorization
         public List<long> GetKeys(long subjectId, long entityId, RightType rightType)
         {
             var subject = SubjectRepository.Get(subjectId);
+            if (subject == null)
+                return new List<long>();
+
             var entity = EntityRepository.Get(entityId);
+            if (entity == null)
+                return new List<long>();
 
             return EntityPermissionRepository.Query(e =>
                 e.Subject.Id == subject.Id &&
@@ -138,14 +166,35 @@ namespace BExIS.Security.Services.Authorization
 
         public int GetRights(Subject subject, Entity entity, long key)
         {
+            if (subject == null)
+                return 0;
+
+            if (entity == null)
+                return 0;
+
             var entityPermission = EntityPermissionRepository.Get(m => m.Subject.Id == subject.Id && m.Entity.Id == entity.Id).FirstOrDefault();
             return entityPermission?.Rights ?? 0;
         }
 
         public List<RightType> GetRights<T>(string subjectName, string entityName, Type entityType, long key) where T : Subject
         {
+            if (string.IsNullOrEmpty(subjectName))
+                return new List<RightType>();
+
+            if (string.IsNullOrEmpty(entityName))
+                return new List<RightType>();
+
+            if (entityType == null)
+                return new List<RightType>();
+
             var subject = SubjectRepository.Query(s => s.Name.ToUpperInvariant() == subjectName.ToUpperInvariant() && s is T).FirstOrDefault();
+            if (subject == null)
+                return new List<RightType>();
+
             var entity = EntityRepository.Query(e => e.Name.ToUpperInvariant() == entityName.ToUpperInvariant() && e.EntityType == entityType).FirstOrDefault();
+            if (entity == null)
+                return new List<RightType>();
+
             return GetRights(subject, entity, key).ToRightTypes();
         }
 
