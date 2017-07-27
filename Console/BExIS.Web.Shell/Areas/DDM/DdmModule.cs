1550f0f0bdc73c9661c2223e29ac5a337242c913
Sven Thiel <sven.thiel@uni-jena.de>
2017-07-27 13:44:52 +0200

    remove errors and commented out ddm seed data generator

--- Console/BExIS.Web.Shell/Areas/DDM/DdmModule.cs
+++ Console/BExIS.Web.Shell/Areas/DDM/DdmModule.cs
@@ -1,5 +1,4 @@
-﻿using BExIS.Modules.Ddm.UI.Helpers;
-using System;
+﻿using System;
 using Vaiona.Logging;
 using Vaiona.Web.Mvc.Modularity;
 
@@ -18,7 +17,12 @@ namespace BExIS.Modules.Ddm.UI
             try
             {
                 base.Install();
-                DdmSeedDataGenerator.GenerateSeedData();
+                // ToDo: refactor the seed data generator
+                // at BExIS.Modules.Ddm.UI.Helpers.DdmSeedDataGenerator.<>c__DisplayClass0_0.<GenerateSeedData>b__1(Feature f) in C:\Users\standard\Source\BExIS\Core\Repos\Code\Console\BExIS.Web.Shell\Areas\DDM\Helpers\DdmSeedDataGenerator.cs:line 25
+                // at System.Linq.Enumerable.FirstOrDefault[TSource](IEnumerable`1 source, Func`2 predicate)
+                // at BExIS.Modules.Ddm.UI.Helpers.DdmSeedDataGenerator.GenerateSeedData() in C: \Users\standard\Source\BExIS\Core\Repos\Code\Console\BExIS.Web.Shell\Areas\DDM\Helpers\DdmSeedDataGenerator.cs:line 25
+                // at BExIS.Modules.Ddm.UI.DdmModule.Install() in C: \Users\standard\Source\BExIS\Core\Repos\Code\Console\BExIS.Web.Shell\Areas\DDM\DdmModule.cs:line 21
+                //DdmSeedDataGenerator.GenerateSeedData();
             }
             catch (Exception e)
             {
