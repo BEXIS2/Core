df727e7c0543eaef2951ab3c0d6952e4e14f8bcc
Sven Thiel <sven.thiel@uni-jena.de>
2017-08-08 17:05:05 +0200

    added seed data generator for system core incl. initial creation of groups

---
+++ Console/BExIS.Web.Shell/Helpers/SeedDataGenerator.cs
@@ -0,0 +1,19 @@
+﻿using BExIS.Security.Services.Subjects;
+
+namespace BExIS.Web.Shell.Helpers
+{
+    public class SeedDataGenerator
+    {
+        public static void Init()
+        {
+            var groupManager = new GroupManager();
+
+            // System Groups
+            if (!groupManager.Exists("everyone", true))
+                groupManager.Create("everyone", "This group represents all registered users.", true);
+
+            if (!groupManager.Exists("anonymous", true))
+                groupManager.Create("anonymous", "This group represents all users (incl. both, registered and non-registered users).", true);
+        }
+    }
+}
