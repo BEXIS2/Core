01f0b6ca24a92df6aaa22bbdfe6a88bc4d2d774b
Sven Thiel <sven.thiel@uni-jena.de>
2017-08-15 16:05:43 +0200

    remove workflow from entities, features and operations are connected to each other directy, again

--- Components/AAA/BExIS.Security.Entities/Objects/Operation.cs
+++ Components/AAA/BExIS.Security.Entities/Objects/Operation.cs
@@ -1,35 +1,12 @@
-﻿using System.Collections.Generic;
-using Vaiona.Entities.Common;
+﻿using Vaiona.Entities.Common;
 
 namespace BExIS.Security.Entities.Objects
 {
     public class Operation : BaseEntity
     {
-        public Operation()
-        {
-            Children = new List<Operation>();
-        }
-
         public virtual string Action { get; set; }
-
-        public virtual ICollection<Operation> Ancestors
-        {
-            get
-            {
-                var ancestors = new List<Operation>();
-
-                if (Parent == null) return ancestors;
-
-                ancestors.Add(Parent);
-                ancestors.AddRange(Parent.Ancestors);
-                return ancestors;
-            }
-        }
-
-        public virtual ICollection<Operation> Children { get; set; }
         public virtual string Controller { get; set; }
         public virtual string Module { get; set; }
-        public virtual Operation Parent { get; set; }
-        public virtual Workflow Workflow { get; set; }
+        public virtual Feature Feature { get; set; }
     }
 }
