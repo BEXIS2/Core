e0c02262b139df2fa6bb6f8fdb458565e15e5360
Sven Thiel <sven.thiel@uni-jena.de>
2017-08-16 17:09:27 +0200

    improve authorization checks

--- Components/EXT/BExIS.Ext.Services/AuthorizationDelegationImplementor.cs
+++ Components/EXT/BExIS.Ext.Services/AuthorizationDelegationImplementor.cs
@@ -1,13 +1,13 @@
-﻿using BExIS.Security.Services.Objects;
+﻿using BExIS.Security.Services.Authorization;
+using BExIS.Security.Services.Objects;
 using BExIS.Security.Services.Subjects;
 using System;
-using BExIS.Security.Services.Authorization;
 
 namespace BExIS.Ext.Services
 {
     public static class AuthorizationDelegationImplementor
     {
-        public static async void CheckAuthorization(string areaName, string controllerName, string actionName, string username, bool isAuthenticated)
+        public static void CheckAuthorization(string areaName, string controllerName, string actionName, string username, bool isAuthenticated)
         {
             // validate the call using the extensibility information (modules, tasks, actions, etc)
             // Call security authorization api utilizing the IoC, Singleton lifetime
@@ -28,10 +28,10 @@ namespace BExIS.Ext.Services
             if (feature == null) return;
 
             var userManager = new UserManager(new UserStore());
-            var user = await userManager.FindByNameAsync(username);
+            var result = userManager.FindByNameAsync(username);
 
             var featurePermissionManager = new FeaturePermissionManager();
-            if (!featurePermissionManager.HasAccess(user, feature))
+            if (!featurePermissionManager.HasAccess(result.Result, feature))
             {
                 throw new UnauthorizedAccessException();
             }
