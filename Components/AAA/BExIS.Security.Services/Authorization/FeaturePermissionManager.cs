4ff8dda47078b03804d80a4fc82c2b1c3715e2ab
Sven Thiel <sven.thiel@uni-jena.de>
2017-08-16 14:02:23 +0200

    remove last workflow methods and update seed data generators for all modules, add new functions for operation manager

--- Components/AAA/BExIS.Security.Services/Authorization/FeaturePermissionManager.cs
+++ Components/AAA/BExIS.Security.Services/Authorization/FeaturePermissionManager.cs
@@ -211,7 +211,7 @@ namespace BExIS.Security.Services.Authorization
         {
             var operation = OperationRepository.Query(x => x.Module.ToUpperInvariant() == module.ToUpperInvariant() && x.Controller.ToUpperInvariant() == controller.ToUpperInvariant() && x.Action.ToUpperInvariant() == action.ToUpperInvariant()).FirstOrDefault();
 
-            var feature = operation?.Workflow.Feature;
+            var feature = operation?.Feature;
             var subject = SubjectRepository.Query(s => s.Name.ToUpperInvariant() == subjectName.ToUpperInvariant() && s is T).FirstOrDefault();
             if (feature != null && subject != null)
                 return HasAccess(subject.Id, feature.Id);
