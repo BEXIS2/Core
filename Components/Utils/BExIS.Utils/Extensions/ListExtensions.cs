1a53116baa3c5e469021e9c8c322c1fe5411d628
Sven Thiel <sven.thiel@uni-jena.de>
2017-09-07 14:06:41 +0200

    update entity permissions management and rights management (using bitwise comparison instead of castings)

--- Components/Utils/BExIS.Utils/Extensions/ListExtensions.cs
+++ Components/Utils/BExIS.Utils/Extensions/ListExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using BExIS.Security.Entities.Authorization;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,19 +8,7 @@ namespace BExIS.Utils.Extensions
     {
         public static int ToInt(this List<RightType> rights)
         {
-            return rights.Count != rights.Distinct().Count() ? 0 : rights.Select(r => Math.Pow(2, (int)r)).ToList().Sum(Convert.ToInt32);
-        }
-
-        public static bool[] ToBoolArray(this List<RightType> rights)
-        {
-            var array = new bool[Enum.GetValues(typeof(RightType)).Length];
-
-            foreach (var rightType in Enum.GetValues(typeof(RightType)).Cast<RightType>())
-            {
-                array[(int)rightType] = rights.Contains(rightType);
-            }
-
-            return array;
+            return rights.Aggregate(0, (current, right) => current | (int)right);
         }
     }
 }
