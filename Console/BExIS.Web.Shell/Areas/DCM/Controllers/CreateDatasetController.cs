5055a64f5f30c3b4ef1a7cd17d67e370bd1bafb7
Javad Chamanara <javad.chamanara@uni-jena.de>
2017-11-14 09:15:21 +0100

    Avoid refreshing MVs when only the metadata part of a dataset is created/updated. #1712

--- Console/BExIS.Web.Shell/Areas/DCM/Controllers/CreateDatasetController.cs
+++ Console/BExIS.Web.Shell/Areas/DCM/Controllers/CreateDatasetController.cs
@@ -522,7 +522,7 @@ namespace BExIS.Modules.Dcm.UI.Controllers
                         TaskManager.AddToBus(CreateTaskmanager.ENTITY_ID, datasetId);
 
                         dm.EditDatasetVersion(workingCopy, null, null, null);
-                        dm.CheckInDataset(datasetId, "Metadata was submited.", GetUsernameOrDefault());
+                        dm.CheckInDataset(datasetId, "Metadata was submited.", GetUsernameOrDefault(), ViewCreationBehavior.None);
 
                         //add to index
                         // ToDo check which SearchProvider it is, default luceneprovider
