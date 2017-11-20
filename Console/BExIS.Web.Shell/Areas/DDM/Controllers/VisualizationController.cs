f2e578a077a3b81d071794e240c02b04262e23c0
Nafiseh Navabpour <nafiseh.navabpour@uni-jena.de>
2017-11-20 15:16:57 +0100

    Add Controller and View for Visualization

---
+++ Console/BExIS.Web.Shell/Areas/DDM/Controllers/VisualizationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.Ddm.UI.Controllers
+{
+    public class VisualizationController : Controller
+    {
+        // GET: Visualization
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
