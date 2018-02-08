d8654e72f0262a6977ac4316d8e093964a0b7f0f
Nafiseh Navabpour <navabpourn@gmail.com>
2018-02-08 11:34:33 +0100

    Add Controller, Model and View into VIM for visualization

---
+++ Console/BExIS.Web.Shell/Areas/VIM/Models/VisualizationModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BExIS.Dlm.Entities.Data;
+
+namespace BExIS.Modules.Vim.UI.Models
+{
+    public class DatasetModels
+    {
+        public string ContentContact { get; set; }
+        public long Id { get; set; }
+
+        public string Owner { get; set; }
+        public string TechnicalContact { get; set; }
+        public string Title { get; set; }
+        public int Version { get; set; }
+
+        public static DatasetModels Convert(Dataset dataset)
+        {
+            return new DatasetModels()
+            {
+                Id = dataset.Id,
+                Version = dataset.VersionNo,
+                Title = dataset.Versions.Last().Metadata.SelectNodes("Metadata/Description/Description/Title/Title")[0].InnerText,
+                TechnicalContact = dataset.Versions.Last().Metadata.SelectNodes("Metadata/TechnicalContact/Person/Name/Name")[0].InnerText,
+                ContentContact = dataset.Versions.Last().Metadata.SelectNodes("Metadata/ContentContact/Person/Name/Name")[0].InnerText,
+                Owner = dataset.Versions.Last().Metadata.SelectNodes("Metadata/Owner/Owner/FullName/Name")[0].InnerText
+            };
+        }
+    }
+
+    public class VisualizationModels
+    {
+        public Dictionary<string, int> allDatasets = new Dictionary<string, int>();
+        public Dictionary<string, int> checkedInDatasets = new Dictionary<string, int>();
+        public Dictionary<string, int> checkedOutDatasets = new Dictionary<string, int>();
+        public Dictionary<string, int> deletedDatasets = new Dictionary<string, int>();
+    }
+}
