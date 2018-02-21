fc7c4a121a6032d7872e1605b1a3199e69ad8125
Javad Chamanara <javad.chamanara@uni-jena.de>
2018-02-21 10:38:53 +0100

    Unit Test project for Shell

---
+++ Console/BExIS.Web.Shell.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using BExIS.App.Testing;
+using BExIS.Utils.Config;
+using NUnit.Framework;
+using System.Web.Mvc;
+using Vaiona.Web.Extensions;
+
+namespace BExIS.Web.Shell.Controllers.Tests
+{
+    [TestFixture()]
+    public class HomeControllerTests
+    {
+        private TestSetupHelper helper = null;
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            helper = new TestSetupHelper(WebApiConfig.Register, false);
+        }
+
+        [Test()]
+        public void IndexTest()
+        {
+            HomeController controller = new HomeController();
+            controller.ControllerContext = helper.BuildHttpContext();
+            ViewResult result = controller.Index() as ViewResult;
+
+            Assert.That(result, Is.Not.Null);
+
+            Assert.That(result.ViewBag.Title, Is.Not.Null);
+
+            string tenantName = controller.ControllerContext.HttpContext.Session.GetTenant().ShortName;
+            Assert.That(result.ViewBag.Title.Contains(tenantName));
+        }
+
+        [Test()]
+        public void SessionTimeoutTest()
+        {
+            HomeController controller = new HomeController();
+            controller.ControllerContext = helper.BuildHttpContext();
+            ViewResult result = controller.SessionTimeout() as ViewResult;
+
+            Assert.That(result, Is.Not.Null);
+
+            Assert.That(result.ViewBag.Title, Is.Not.Null);
+
+            string tenantName = controller.ControllerContext.HttpContext.Session.GetTenant().ShortName;
+            Assert.That(result.ViewBag.Title.Contains(tenantName));
+        }
+    }
+}
