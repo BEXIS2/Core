176ee422a7045b916fc7f5fcb484892713d37c9e
David Blaa <dblaa@bgc-jena.mpg.de>
2018-04-25 14:59:44 +0200

    Issue #22 - use DateTime display pattern as a pattern for import data - add custom pattern to the excel template - Update DisplayPattern

--- Components/IO/BExIS.Io/IOUtility.cs
+++ Components/IO/BExIS.Io/IOUtility.cs
@@ -33,6 +33,54 @@ namespace BExIS.IO
             return false;
         }
 
+        public static bool IsDate(string value, out DateTime dateTime)
+        {
+ 
+            if (DateTime.TryParse(value, out dateTime))
+            {
+                return true;
+            }
+
+            if (DateTime.TryParse(value, new CultureInfo("en-US", false), DateTimeStyles.None, out dateTime))
+            {
+                return true;
+            }
+
+            if (DateTime.TryParse(value, new CultureInfo("de-DE", false), DateTimeStyles.None, out dateTime))
+            {
+                return true;
+            }
+
+            if (DateTime.TryParse(value, CultureInfo.InvariantCulture, DateTimeStyles.None, out dateTime))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// try to convert a string with a pattern to a datetime
+        /// </summary>
+        /// <param name="dateAsString"></param>
+        /// <param name="pattern"></param>
+        /// <param name="dateTime"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns>true or false and out datetime</returns>
+        public static bool IsDate(string dateAsString, string pattern, out DateTime dateTime, CultureInfo cultureInfo = null)
+        {
+
+            if (cultureInfo == null) cultureInfo = CultureInfo.InvariantCulture;
+
+            if (ConvertToDate(dateAsString, pattern, out dateTime, cultureInfo))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
         /// <summary>
         /// Convert a Datetime as string to a datetime as string from a other culture.
         /// </summary>
@@ -81,5 +129,49 @@ namespace BExIS.IO
             return null;
         }
 
+
+        /// <summary>
+        /// try to convert a string with a pattern to a datetime
+        /// </summary>
+        /// <param name="dateAsString"></param>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public static string ConvertToDateUS(string dateAsString, string pattern, CultureInfo cultureInfo = null)
+        {
+            DateTime tmp;
+            if (cultureInfo == null) cultureInfo = CultureInfo.InvariantCulture;
+
+            if (ConvertToDate(dateAsString, pattern, out tmp, cultureInfo))
+            {
+                return tmp.ToString(new CultureInfo("en-us"));
+            }
+
+            return "";
+        }
+
+        public static bool ConvertToDate(string dateAsString, string pattern, out DateTime dateTime ,CultureInfo cultureInfo = null)
+        {
+            if (cultureInfo == null) cultureInfo = CultureInfo.InvariantCulture;
+
+            if (DateTime.TryParseExact(dateAsString, pattern, cultureInfo, DateTimeStyles.NoCurrentDateDefault, out dateTime))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static string ExportDateTimeString(string dateAsString, string pattern, out DateTime dateTime, CultureInfo cultureInfo = null)
+        {
+     
+            if (DateTime.TryParse(dateAsString, new CultureInfo("en-US", false), DateTimeStyles.NoCurrentDateDefault, out dateTime))
+            {
+                return dateTime.ToString(pattern);
+            }
+
+            return "";
+        }
+
+        
     }
 }
