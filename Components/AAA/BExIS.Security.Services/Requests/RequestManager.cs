70719e51432ec0f5204751843ce619d8f1e1a63e
Sven Thiel <sven.thiel@uni-jena.de>
2018-06-18 11:40:26 +0200

    #31 +add first draft of function to add a dataset request

--- Components/AAA/BExIS.Security.Services/Requests/RequestManager.cs
+++ Components/AAA/BExIS.Security.Services/Requests/RequestManager.cs
@@ -1,4 +1,8 @@
-﻿using BExIS.Security.Entities.Requests;
+﻿using BExIS.Dlm.Entities.Party;
+using BExIS.Security.Entities.Objects;
+using BExIS.Security.Entities.Requests;
+using BExIS.Security.Entities.Subjects;
+using System;
 using System.Linq;
 using Vaiona.Persistence.Api;
 
@@ -14,7 +18,48 @@ namespace BExIS.Security.Services.Requests
         }
 
         public IReadOnlyRepository<Request> RequestRepository { get; }
+        public IReadOnlyRepository<Party> PartyRepository { get; }
         public IQueryable<Request> Requests => RequestRepository.Query();
+
+        public void Create(long applicantId, long entityId, long key, int rights = 1)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var requestRepository = uow.GetRepository<Request>();
+                var decisionRepository = uow.GetRepository<Decision>();
+                var partyRelationshipRepository = uow.GetReadOnlyRepository<PartyRelationship>();
+                var partyUserRepository = uow.GetReadOnlyRepository<PartyUser>();
+                var userRepository = uow.GetReadOnlyRepository<User>();
+                var entityRepository = uow.GetReadOnlyRepository<Entity>();
+
+                var firstOrDefault = partyRelationshipRepository.Query(m => m.PartyRelationshipType.Title == "Owner" && m.SecondParty.Id == key).FirstOrDefault();
+
+                if (firstOrDefault == null) return;
+
+                var partyUser = partyUserRepository.Get(firstOrDefault.FirstParty.Id);
+
+                var request = new Request()
+                {
+                    Applicant = userRepository.Get(applicantId),
+                    Entity = entityRepository.Get(entityId),
+                    Key = key,
+                    RequestDate = DateTime.Now,
+                    Status = RequestStatus.Open
+                };
+
+                requestRepository.Put(request);
+
+                var decision = new Decision()
+                {
+                    Status = DecisionStatus.Open,
+                    Request = request,
+                    DecisionMaker = userRepository.Get(partyUser.UserId)
+                };
+
+                decisionRepository.Put(decision);
+                uow.Commit();
+            }
+        }
 
         public void Create(Request request)
         {
