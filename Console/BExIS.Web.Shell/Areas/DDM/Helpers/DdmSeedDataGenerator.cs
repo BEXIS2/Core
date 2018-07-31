5521a8bc3843b560103f729870302b8a93a85cc9
David Blaa <dblaa@bgc-jena.mpg.de>
2018-07-31 14:31:08 +0200

    - seperate controller for search and public search - set search home controller  operation to search feature

--- Console/BExIS.Web.Shell/Areas/DDM/Helpers/DdmSeedDataGenerator.cs
+++ Console/BExIS.Web.Shell/Areas/DDM/Helpers/DdmSeedDataGenerator.cs
@@ -7,7 +7,7 @@ using Vaiona.Web.Mvc.Modularity;
 
 namespace BExIS.Modules.Ddm.UI.Helpers
 {
-    public class DdmSeedDataGenerator: IModuleSeedDataGenerator
+    public class DdmSeedDataGenerator : IModuleSeedDataGenerator
     {
         public void GenerateSeedData()
         {
@@ -80,7 +80,8 @@ namespace BExIS.Modules.Ddm.UI.Helpers
                 // I had to remove the feature to get dashboard running without DDM feature permissions.
                 // We have to think about how we can fix it in a long run. Maybe "DDM/Home" is not the proper
                 // place for dashboard!?
-                operationManager.Create("DDM", "Home", "*"); //, SearchFeature);
+                operationManager.Create("DDM", "PublicSearch", "*", SearchFeature);
+                operationManager.Create("DDM", "Home", "*", SearchFeature);
                 operationManager.Create("DDM", "Data", "*", SearchFeature);
 
                 #endregion
