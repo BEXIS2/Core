d3504d935a19dde2a9e0ab755e42ef41dc90782e
David Blaa <dblaa@bgc-jena.mpg.de>
2018-08-07 15:54:55 +0200

    #76 fix long dirty issue with the primary data view for many variables. also fix the telerik grid filter issue in the header.

--- Console/BExIS.Web.Shell/Areas/DCM/Controllers/EasyUploadVerificationController.cs
+++ Console/BExIS.Web.Shell/Areas/DCM/Controllers/EasyUploadVerificationController.cs
@@ -362,10 +362,20 @@ namespace BExIS.Modules.Dcm.UI.Controllers
                         dataAttributeInfos = dataAttributeInfos.Where(d => d.Id.Equals(currentDataAttrInfo.Id)).ToList();
 
                         //filtering units when data attr is selected, if id or dimension is the same
-                        if (selectedUnit == 0) unitInfos = unitInfos.Where(u => u.UnitId.Equals(currentDataAttrInfo.UnitId) || u.DimensionId.Equals(currentDataAttrInfo.DimensionId)).ToList();
+                        if (selectedUnit == 0)
+                        {
+                            unitInfos = unitInfos.Where(u => u.UnitId.Equals(currentDataAttrInfo.UnitId) || u.DimensionId.Equals(currentDataAttrInfo.DimensionId)).ToList();
+                            currentUnit = unitInfos.FirstOrDefault(u => u.UnitId.Equals(currentDataAttrInfo.UnitId));
+                        }
                         else unitInfos = unitInfos.Where(u => u.UnitId.Equals(currentUnit.UnitId) || u.DimensionId.Equals(currentUnit.DimensionId)).ToList();
 
-                        if (selectedDataType == 0) dataTypeInfos = unitInfos.SelectMany(u => u.DataTypeInfos).GroupBy(d => d.DataTypeId).Select(g => g.Last()).ToList();
+                        if (selectedDataType == 0)
+                        {
+                            dataTypeInfos = unitInfos.SelectMany(u => u.DataTypeInfos).GroupBy(d => d.DataTypeId).Select(g => g.Last()).ToList();
+
+                            currentDataTypeInfo = dataTypeInfos.FirstOrDefault(d => d.DataTypeId.Equals(currentDataAttrInfo.DataTypeId));
+
+                        }
                         else dataTypeInfos = dataTypeInfos.Where(dt => dt.DataTypeId.Equals(currentDataTypeInfo.DataTypeId)).ToList();
                     }
 
