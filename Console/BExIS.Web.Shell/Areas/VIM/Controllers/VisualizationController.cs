f20238eecb3287cd4008bf7ee29624f1fd08eef5
Nafiseh Navabpour <navabpourn@gmail.com>
2018-08-27 08:57:36 +0200

    Change creation date calculation for visualization

--- Console/BExIS.Web.Shell/Areas/VIM/Controllers/VisualizationController.cs
+++ Console/BExIS.Web.Shell/Areas/VIM/Controllers/VisualizationController.cs
@@ -18,15 +18,19 @@ namespace BExIS.Modules.Vim.UI.Controllers
             //ViewBag.Title = PresentationModel.GetViewTitleForTenant("Maintain Datasets", Session.GetTenant());
             VisualizationModels visModel = new VisualizationModels();
             Dictionary<string, int> allDatasets = new Dictionary<string, int>();
-            Dictionary<string, int> checkedInDatasets = new Dictionary<string, int>();
-            Dictionary<string, int> checkedOutDatasets = new Dictionary<string, int>();
+            Dictionary<string, int> createdDatasets = new Dictionary<string, int>();
+            //Dictionary<string, int> checkedInDatasets = new Dictionary<string, int>();
+            //Dictionary<string, int> checkedOutDatasets = new Dictionary<string, int>();
             Dictionary<string, int> deletedDatasets = new Dictionary<string, int>();
+            List<string> helpCount = new List<string>(); //It is later used to save date of creation of a dataset (or similar for other types). It helps to save dates to count them later.
 
             DatasetManager dm = new DatasetManager();
             var entityPermissionManager = new EntityPermissionManager();
 
             List<Dataset> datasets = dm.DatasetRepo.Query().OrderBy(p => p.Id).ToList();
-
+            List<long> datasetIds = datasets.Select(p => p.Id).ToList();
+            
+            ///Find deleted datasets
             var groupsByYear = datasets.GroupBy(p => p.LastCheckIOTimestamp.Year);
             foreach (var year in groupsByYear)
             {
@@ -45,20 +49,41 @@ namespace BExIS.Modules.Vim.UI.Controllers
                     {
                         countGroup = status.Count();
                         string statusofGroup = status.First().Status.ToString();
-                        if (statusofGroup == "CheckedIn")
-                        { checkedInDatasets.Add(dateOfGroup, countGroup); }
-                        if (statusofGroup == "CheckedOut")
-                        { checkedOutDatasets.Add(dateOfGroup, countGroup); }
-                        if (statusofGroup == "Deleted")
+                        //if (statusofGroup == "CheckedIn")
+                        //{ checkedInDatasets.Add(dateOfGroup, countGroup); }
+                        //if (statusofGroup == "CheckedOut")
+                        //{ checkedOutDatasets.Add(dateOfGroup, countGroup); }
+                        //if (statusofGroup == "Deleted")
                         { deletedDatasets.Add(dateOfGroup, countGroup); }
                     }
 
                 }
             }
 
+            ///Find Created Datasets
+            foreach(var id in datasetIds)
+            {
+                List<DatasetVersion> versions = dm.DatasetVersionRepo.Query(p => p.Dataset.Id == id).OrderBy(p => p.Id).ToList();                
+                //extract the timestamp of version id = 1. This is the time of the creation of a dataset.
+                var createTime = versions.First().Timestamp.Month + "/" + versions.First().Timestamp.Year;
+                helpCount.Add(createTime);
+            }
+
+            var items = from tt in helpCount
+                        group tt by tt into g
+                        let count = g.Count()
+                        orderby count descending
+                        select new { Value = g.Key, Count = count };
+            
+            foreach (var t in items)
+            {
+                createdDatasets.Add(t.Value, t.Count);
+            }
+
+            
             visModel.allDatasets = allDatasets;
-            visModel.checkedInDatasets = checkedInDatasets;
-            visModel.checkedOutDatasets = checkedOutDatasets;
+            //visModel.checkedInDatasets = checkedInDatasets;
+            //visModel.checkedOutDatasets = checkedOutDatasets;
             visModel.deletedDatasets = deletedDatasets;
 
             return View(visModel);
