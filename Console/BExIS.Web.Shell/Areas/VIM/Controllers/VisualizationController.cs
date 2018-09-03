6d18c0bf1e0628c6bbf30302d647509954164ec3
Nafiseh Navabpour <navabpourn@gmail.com>
2018-09-03 09:45:22 +0200

    solve issue #153 and monthly count issue

--- Console/BExIS.Web.Shell/Areas/VIM/Controllers/VisualizationController.cs
+++ Console/BExIS.Web.Shell/Areas/VIM/Controllers/VisualizationController.cs
@@ -17,60 +17,98 @@ namespace BExIS.Modules.Vim.UI.Controllers
         {
             //ViewBag.Title = PresentationModel.GetViewTitleForTenant("Maintain Datasets", Session.GetTenant());
             VisualizationModels visModel = new VisualizationModels();
-            Dictionary<string, int> allDatasets = new Dictionary<string, int>();
+            //DataInfo infModel = new DataInfo();
+            //Dictionary<string, int> allDatasets = new Dictionary<string, int>();
+            Dictionary<string, int> allActivities = new Dictionary<string, int>();
             Dictionary<string, int> createdDatasets = new Dictionary<string, int>();
-            //Dictionary<string, int> checkedInDatasets = new Dictionary<string, int>();
-            //Dictionary<string, int> checkedOutDatasets = new Dictionary<string, int>();
             Dictionary<string, int> deletedDatasets = new Dictionary<string, int>();
             List<string> helpCountCreated = new List<string>(); //It is later used to save date of creation of a dataset (or similar for other types). It helps to save dates to count them later.
             List<string> helpCountDeleted = new List<string>();
+            List<string> helpCountActivity = new List<string>();
 
+            //--------
+            
             DatasetManager dm = new DatasetManager();
             var entityPermissionManager = new EntityPermissionManager();
 
             List<Dataset> datasets = dm.DatasetRepo.Query().OrderBy(p => p.Id).ToList();
-            List<long> datasetIds = datasets.Select(p => p.Id).ToList();            
+            List<long> datasetIds = datasets.Select(p => p.Id).ToList();
 
-            foreach(var id in datasetIds)
+            foreach (var id in datasetIds)
             {
-                List<DatasetVersion> versions = dm.DatasetVersionRepo.Query(p => p.Dataset.Id == id).OrderBy(p => p.Id).ToList();                
+                Dataset dataset = dm.GetDataset(id);
+
+                List<DatasetVersion> versions = dm.DatasetVersionRepo.Query(p => p.Dataset.Id == id).OrderBy(p => p.Id).ToList();
+
+                List<long> datasetVersionId = versions.Select(p => p.Id).ToList();
+
                 //extract the timestamp of version id = 1. This is the time of the creation of a dataset.
                 var createTime = versions.First().Timestamp.Month + "/" + versions.First().Timestamp.Year;
                 helpCountCreated.Add(createTime);
+
                 //extract the timestamp of the last version. This is the time of the deletion of a dataset.
-                var deleteTime = versions.Last().Timestamp.Month + "/" + versions.First().Timestamp.Year;
-                helpCountDeleted.Add(deleteTime);
+                // if(dataset.Status == DatasetStatus.Deleted) -- BY DAVID
+                if (dataset.Status == DatasetStatus.Deleted)
+                {
+                    var deleteTime = versions.Last().Timestamp.Month + "/" + versions.Last().Timestamp.Year;
+                    helpCountDeleted.Add(deleteTime);
+                }
+
+                foreach (var version in versions)
+                {
+                    var activityTime = version.Timestamp.Month + "/" + version.Timestamp.Year;
+                    helpCountActivity.Add(activityTime);
+                    
+                }
+
             }
 
+            ///Create the list of created datasets
             var createdItems = from tt in helpCountCreated
-                                group tt by tt into g
-                                let count = g.Count()
-                                orderby count ascending
-                                select new { Value = g.Key, Count = count };
+                               group tt by tt into g
+                               let count = g.Count()
+                               orderby count ascending
+                               select new { Value = g.Key, Count = count };
 
             foreach (var t in createdItems)
             {
                 createdDatasets.Add(t.Value, t.Count);
             }
 
+            //createdDatasets.OrderBy(order => order.Value).ToList();
+
+            ///Create the list of deleted datasets
             var deletedItems = from tt in helpCountDeleted
-                                group tt by tt into g
-                                let count = g.Count()
-                                orderby count ascending
-                                select new { Value = g.Key, Count = count };
+                               group tt by tt into g
+                               let count = g.Count()
+                               orderby count ascending
+                               select new { Value = g.Key, Count = count };
 
             foreach (var t in deletedItems)
             {
                 deletedDatasets.Add(t.Value, t.Count);
             }
 
+            ///Create the list of all activities includes create, update and delete
+            var activityItems = from tt in helpCountActivity
+                                group tt by tt into g
+                                let count = g.Count()
+                                orderby count ascending
+                                select new { Value = g.Key, Count = count };
 
-            visModel.allDatasets = allDatasets;
+            foreach (var t in activityItems)
+            {
+                allActivities.Add(t.Value, t.Count);
+            }
+
+            visModel.allActivities = allActivities;
+            visModel.allDatasets = allActivities;
             visModel.createdDatasets = createdDatasets;
             //visModel.checkedInDatasets = checkedInDatasets;
             //visModel.checkedOutDatasets = checkedOutDatasets;
             visModel.deletedDatasets = deletedDatasets;
 
+            
             return View(visModel);
         }
     }
