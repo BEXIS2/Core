3a7026cbc6c3bb189b9ebe8e91087596cde42110
DavidBlaa <dblaa@bgc-jena.mpg.de>
2019-09-10 14:24:26 +0200

    change the application root for test projects

--- Components/Utils/BExIS.Utils/Config/WebApiConfig.cs
+++ Components/Utils/BExIS.Utils/Config/WebApiConfig.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http;
+using Vaiona.Utils.Cfg;
 
 namespace BExIS.Utils.Config
 {
     public static class WebApiConfig
     {
         private static bool isRegistered; // this is to prevent double registering the routes during unit testing the Application's Global class
+
         public static void Register(HttpConfiguration config)
         {
             if (isRegistered)
