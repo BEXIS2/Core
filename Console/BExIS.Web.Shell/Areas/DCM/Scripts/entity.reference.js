47fff91904d60537d3fb79b1a73a284c7ae559df
Franziska Zander <franzi82@gmx.de>
2019-10-19 19:06:00 +0200

    Select first item in the list (now sorted descending) #335

--- Console/BExIS.Web.Shell/Areas/DCM/Scripts/entity.reference.js
+++ Console/BExIS.Web.Shell/Areas/DCM/Scripts/entity.reference.js
@@ -73,7 +73,7 @@ function successFuncTargetVersions(data, status) {
     }
 
     $("#TargetVersion").prop("disabled", false);
-    $("#TargetVersion").prop("selectedIndex", data.length - 1);
+    $("#TargetVersion").prop("selectedIndex", 0);
     $("#TargetVersion").removeClass("bx-disabled");
 }
 
