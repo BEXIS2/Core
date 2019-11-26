699b9078ec9257a313ed0c63dfd9cec38a438966
David Blaa <david.blaa@googlemail.com>
2019-11-26 15:55:32 +0100

    # ddm manual is now loaded from the git as a external link

--- Console/BExIS.Web.Shell/Areas/DDM/Controllers/HelpController.cs
+++ Console/BExIS.Web.Shell/Areas/DDM/Controllers/HelpController.cs
@@ -1,6 +1,14 @@
 ﻿using System.Web.Mvc;
 using Vaiona.Web.Mvc.Models;
 using Vaiona.Web.Extensions;
+using System.IO;
+using Vaiona.Utils.Cfg;
+using System.Xml;
+using BExIS.Xml.Helpers;
+using System.Xml.Linq;
+using Markdig;
+using System.Net;
+using System.Web;
 
 namespace BExIS.Modules.Ddm.UI.Controllers
 {
@@ -11,10 +19,27 @@ namespace BExIS.Modules.Ddm.UI.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.Title = PresentationModel.GetViewTitleForTenant("Search Manual", this.Session.GetTenant());
 
-            return View();
+            string filePath = Path.Combine(AppConfiguration.GetModuleWorkspacePath("DDM"), "Ddm.Settings.xml");
+            XDocument settings = XDocument.Load(filePath);
+            XElement help = XmlUtility.GetXElementByAttribute("entry", "key", "help", settings);
+
+            string helpurl = help.Attribute("value")?.Value;
+
+            //ServicePointManager.Expect100Continue = true;
+            //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            //using (var client = new WebClient())
+            //{
+
+            //    var helpfile = client.DownloadString(helpurl);
+
+            //    string model = Markdown.ToHtml(helpfile);
+            //    return View("Index", (object)model);
+            //}
+
+            return Redirect(helpurl);
 
         }
+
     }
 }
