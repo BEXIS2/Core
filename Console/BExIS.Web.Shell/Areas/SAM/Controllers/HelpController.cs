ba6abfe310d1d89d7e039306f176967974d5962a
David Blaa <david.blaa@googlemail.com>
2019-11-27 08:16:55 +0100

    #415 link all manuals to external links

--- Console/BExIS.Web.Shell/Areas/SAM/Controllers/HelpController.cs
+++ Console/BExIS.Web.Shell/Areas/SAM/Controllers/HelpController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using BExIS.Xml.Helpers;
+using System.IO;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Vaiona.Utils.Cfg;
 using Vaiona.Web.Extensions;
 using Vaiona.Web.Mvc.Models;
 
@@ -6,13 +10,17 @@ namespace BExIS.Modules.Sam.UI.Controllers
 {
     public class HelpController : Controller
     {
-        //
-        // GET: /DDM/Help/
-
         public ActionResult Index()
         {
-            ViewBag.Title = PresentationModel.GetViewTitleForTenant("Administration Manual", this.Session.GetTenant());
-            return View();
+            string filePath = Path.Combine(AppConfiguration.GetModuleWorkspacePath("SAM"), "Sam.Settings.xml");
+            XDocument settings = XDocument.Load(filePath);
+            XElement help = XmlUtility.GetXElementByAttribute("entry", "key", "help", settings);
+
+            string helpurl = help.Attribute("value")?.Value;
+
+
+            return Redirect(helpurl);
+
         }
     }
 }
