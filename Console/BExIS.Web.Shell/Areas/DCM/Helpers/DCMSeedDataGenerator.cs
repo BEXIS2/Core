8c5ec0a34d72f70a73ec235d6cc3eab68dc570fa
DavidBlaa <dblaa@bgc-jena.mpg.de>
2019-12-11 16:46:48 +0100

    small fix in seeddata

--- Console/BExIS.Web.Shell/Areas/DCM/Helpers/DCMSeedDataGenerator.cs
+++ Console/BExIS.Web.Shell/Areas/DCM/Helpers/DCMSeedDataGenerator.cs
@@ -97,20 +97,17 @@ namespace BExIS.Modules.Dcm.UI.Helpers
                 }
                 else
                 {
-                    if (entity.Extra == null)
-                    {
-                        //update to Extra
+                    XmlDocument xmlDoc = new XmlDocument();
 
-                        XmlDocument xmlDoc = new XmlDocument();
-                        xmlDoc.AppendChild(entity.Extra);
+                    if (entity.Extra != null) xmlDoc.AppendChild(entity.Extra);
 
-                        XmlDatasetHelper xmlDatasetHelper = new XmlDatasetHelper();
-                        xmlDatasetHelper.AddReferenceToXml(xmlDoc, AttributeNames.name.ToString(), "ddm", AttributeType.parameter.ToString(), "extra/modules/module");
+                    //update to Extra
+                    XmlDatasetHelper xmlDatasetHelper = new XmlDatasetHelper();
+                    xmlDatasetHelper.AddReferenceToXml(xmlDoc, AttributeNames.name.ToString(), "ddm", AttributeType.parameter.ToString(), "extra/modules/module");
 
-                        entity.Extra = xmlDoc;
+                    entity.Extra = xmlDoc;
 
-                        entityManager.Update(entity);
-                    }
+                    entityManager.Update(entity);
                 }
 
                 #endregion create entities
