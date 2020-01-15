fe14f578d3ef6733f85d3d2862640dee196a5671
David Schöne <david.blaa@googlemail.com>
2020-01-15 15:23:01 +0100

    remove history check for the case (2 changes in one edit for one datatuple)

--- Components/DLM/BExIS.Dlm.Services/Data/DatasetManager.cs
+++ Components/DLM/BExIS.Dlm.Services/Data/DatasetManager.cs
@@ -2858,8 +2858,8 @@ namespace BExIS.Dlm.Services.Data
                             if (orginalTuple == null || orginalTuple.Id <= 0) // maybe the tuple is in the edited list by a mistake!
                                 continue;
                             //check if the history record for this data tuple has been created before. in cases of multiple edits in a single version for example
-                            if (dataTupleVersionRepo.Query(p => p.OriginalTuple.Id == orginalTuple.Id && p.DatasetVersion.Id == orginalTuple.DatasetVersion.Id).Count() <= 0) // it is the first time the orginalTuple is getting editedVersion. so add a history record. the history record, keeps the tuple as was before the first edit!
-                            {
+                            //if (dataTupleVersionRepo.Query(p => p.OriginalTuple.Id == orginalTuple.Id && p.DatasetVersion.Id == orginalTuple.DatasetVersion.Id).Count() <= 0) // it is the first time the orginalTuple is getting editedVersion. so add a history record. the history record, keeps the tuple as was before the first edit!
+                            //{
                                 DataTupleVersion tupleVersion = new DataTupleVersion()
                                 {
                                     TupleAction = TupleAction.Edited,
@@ -2878,7 +2878,7 @@ namespace BExIS.Dlm.Services.Data
                                 tupleVersionsTobeAdded.Add(tupleVersion);
                                 //DataTuple merged =
                                 //orginalTuple.History.Add(tupleVersion);
-                            }
+                            //}
 
                             //need a better way to preserve changes during the fetch of the original tuple. Maybe deep copy/ evict/ merge works
                             //XmlDocument xmlVariableValues = new XmlDocument();
