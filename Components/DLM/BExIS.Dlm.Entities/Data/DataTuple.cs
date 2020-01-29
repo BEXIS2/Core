837a33fb2df36ed6cacdca88b13a3bb098ab6ef6
David Blaa <david.blaa@googlemail.com>
2020-01-29 07:35:16 +0100

    add json column

--- Components/DLM/BExIS.Dlm.Entities/Data/DataTuple.cs
+++ Components/DLM/BExIS.Dlm.Entities/Data/DataTuple.cs
@@ -8,13 +8,13 @@ using Vaiona.Logging;
 
 /// <summary>
 ///
-/// </summary>        
+/// </summary>
 namespace BExIS.Dlm.Entities.Data
 {
     /// <summary>
     /// Its to overcome an inheritance issue with NH: when TupleVersion is derived from DataTuple all queries on DataTuple return versions too.
     /// </summary>
-    /// <remarks></remarks>        
+    /// <remarks></remarks>
     [AutomaticMaterializationInfo("VariableValues", typeof(List<VariableValue>), "XmlVariableValues", typeof(XmlDocument))]
     [AutomaticMaterializationInfo("Amendments", typeof(List<Amendment>), "XmlAmendments", typeof(XmlDocument))]
     public abstract class AbstractTuple : BaseEntity, IBusinessVersionedEntity
@@ -25,21 +25,21 @@ namespace BExIS.Dlm.Entities.Data
         /// indicates the order of the associated tuple in the version
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual int OrderNo { get; set; }
 
         /// <summary>
         ///
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual TupleAction TupleAction { get; set; }
 
         /// <summary>
         ///
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual DateTime Timestamp { get; set; }
 
         /// <summary>
@@ -48,64 +48,66 @@ namespace BExIS.Dlm.Entities.Data
         /// every variablevalue knows about its Obtaining method which is one of the items in the DataAttribute ValueType
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual XmlDocument XmlVariableValues { get; set; }
 
         /// <summary>
         ///
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual XmlDocument XmlAmendments { get; set; }
+
+        public virtual String JsonVariableValues { get; set; }
 
         /// <summary>
         ///
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public abstract DataTupleType TupleType { get; }
-        #endregion
-     
-        #region Associations        
+
+        #endregion Attributes
+
+        #region Associations
 
         /// <summary>
         /// inverse map
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual DatasetVersion DatasetVersion { get; set; }
 
         /// <summary>
         /// Map from and to XmlVariableValues. Do not map to persistence data directly
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual IList<VariableValue> VariableValues { get; set; }
 
         /// <summary>
         /// Do not map to persistence data directly. Materialize after load
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public virtual IList<Amendment> Amendments { get; set; }
-        
-        #endregion
 
+        #endregion Associations
     }
-  
+
     /// <summary>
     /// In order to show what had happened to each tuple, a record of the action applied to them is maintained in the version they belong to.
     /// </summary>
     public enum TupleAction
     {
-            Created     =1   // the tuple is created explicitly in this version
-        ,   Edited      =2   // the tuple was from the previous version, but edited here and is attached to this version a new instance keeping the original tuple ID
-        ,   Deleted     =3   // the tuple from the previous version is deleted, and this version is just pointing to that tuple in the previous one to keep track of deleted tuples. it is possible to omit this action
-        ,   Untouched   =4   // the tuple is part of this version without any change. in this case the new version points to the previous one by an "Untouched"  action to prevent duplicating the tuple.
+        Created = 1   // the tuple is created explicitly in this version
+        , Edited = 2   // the tuple was from the previous version, but edited here and is attached to this version a new instance keeping the original tuple ID
+        , Deleted = 3   // the tuple from the previous version is deleted, and this version is just pointing to that tuple in the previous one to keep track of deleted tuples. it is possible to omit this action
+        , Untouched = 4   // the tuple is part of this version without any change. in this case the new version points to the previous one by an "Untouched"  action to prevent duplicating the tuple.
     }
 
     /// <summary>
-    /// 
+    ///
     /// </summary>
     public enum DataTupleType
     {
@@ -116,14 +118,14 @@ namespace BExIS.Dlm.Entities.Data
     /// <summary>
     ///
     /// </summary>
-    /// <remarks></remarks>        
+    /// <remarks></remarks>
     public class DataTuple : AbstractTuple
     {
         /// <summary>
         ///
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         public override DataTupleType TupleType
         {
             get { return DataTupleType.Original; }
@@ -135,19 +137,19 @@ namespace BExIS.Dlm.Entities.Data
         ///
         /// </summary>
         /// <remarks></remarks>
-        /// <seealso cref=""/>        
+        /// <seealso cref=""/>
         //public virtual ICollection<DataTupleVersion> History { get; set; }
 
-        #endregion      
+        #endregion Associations
 
         #region Methods
 
         // No need to override these functions, the base one performs the task for normal cases
         // If you have a very special case or the performance of the generic one is not good, then override the methods
         //public override void Dematerialize()
-        //{            
-        //    XmlVariableValues = (XmlDocument)transformer.ExportTo(VariableValues, "VariableValues", 1);            
-        //    XmlAmendments = (XmlDocument)transformer.ExportTo(Amendments, "Amendments", 1);             
+        //{
+        //    XmlVariableValues = (XmlDocument)transformer.ExportTo(VariableValues, "VariableValues", 1);
+        //    XmlAmendments = (XmlDocument)transformer.ExportTo(Amendments, "Amendments", 1);
         //}
 
         //public override void Materialize()
@@ -161,7 +163,7 @@ namespace BExIS.Dlm.Entities.Data
         /// </summary>
         /// <remarks></remarks>
         /// <seealso cref=""/>
-        /// <param>NA</param>  
+        /// <param>NA</param>
         public DataTuple()
         {
             //XmlVariableValues = new XmlDocument();
@@ -176,12 +178,13 @@ namespace BExIS.Dlm.Entities.Data
         /// </summary>
         /// <remarks></remarks>
         /// <seealso cref=""/>
-        /// <param>NA</param>   
+        /// <param>NA</param>
         public override void Materialize(bool includeChildren = true)
         {
             base.Materialize();
             VariableValues.ToList().ForEach(p => p.Tuple = this);
         }
-        #endregion
+
+        #endregion Methods
     }
 }
