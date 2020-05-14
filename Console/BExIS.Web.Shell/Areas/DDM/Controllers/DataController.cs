e55aba9511f6577ad93dfb710cd903aa8b48a5cb
Franzi <franzi82@gmx.de>
2020-05-14 09:58:23 +0200

    Change style no primary data uploaded #503

--- Console/BExIS.Web.Shell/Areas/DDM/Controllers/DataController.cs
+++ Console/BExIS.Web.Shell/Areas/DDM/Controllers/DataController.cs
@@ -578,7 +578,7 @@ namespace BExIS.Modules.Ddm.UI.Controllers
                             {
                                 long count = dm.RowCount(datasetID, null);
                                 if (count > 0) table = dm.GetLatestDatasetVersionTuples(datasetID, null, null, null, 0, 100);
-                                else ModelState.AddModelError(string.Empty, "There is no primary data available/uploaded. <br/><br/> Please note that the data may have been uploaded to another repository and is referenced here in the metadata.");
+                                else ModelState.AddModelError(string.Empty, "<span style=\"color: black;\"> There is no primary data available/uploaded. </span><br/><br/> <span style=\"font-weight: normal;color: black;\">Please note that the data may have been uploaded to another repository and is referenced here in the metadata.</span>");
                             }
                             catch
                             {
