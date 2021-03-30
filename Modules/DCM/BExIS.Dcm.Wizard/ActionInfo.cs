07c69c8d058658aa74518bb00852f055fa5067c0
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-03-30 08:38:17 +0200

    #721, add check for controller name

--- Modules/DCM/BExIS.Dcm.Wizard/ActionInfo.cs
+++ Modules/DCM/BExIS.Dcm.Wizard/ActionInfo.cs
@@ -60,10 +60,10 @@ namespace BExIS.Dcm.Wizard
         /// <returns></returns>
         public bool IsComplete()
         {
-            if (String.IsNullOrEmpty(ActionName))
-                return false;
+            //if (String.IsNullOrEmpty(AreaName))
+            //    return false;
 
-            if (String.IsNullOrEmpty(ActionName))
+            if (String.IsNullOrEmpty(ControllerName))
                 return false;
 
             if (String.IsNullOrEmpty(ActionName))
