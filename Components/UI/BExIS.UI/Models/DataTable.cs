dc40f76295b2c239617aacf4092e8e8791beb9ca
David Schöne <david.blaa@googlemail.com>
2021-08-11 15:07:53 +0200

    add datatable model and ui test example

---
+++ Components/UI/BExIS.UI/Models/DataTable.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BExIS.UI.Models
+{
+ 
+    public class DataTableSendModel
+    {
+        public int draw { get; set; }
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        public Object data { get; set; }
+        public string error { get; set; }
+
+        public DataTableSendModel()
+        {
+            draw = 0;
+            recordsTotal = 0;
+            recordsFiltered = 0;
+            data = new Object();
+            error = "";
+        }
+    }
+
+    public sealed class DataTablesColumnsModel
+    {
+        public string Data { get; set; }
+        public string Name { get; set; }
+        public bool Orderable { get; set; }
+        public DataTablesSearchModel Search { get; set; }
+        public bool Searchable { get; set; }
+    }
+
+    public sealed class DataTablesOrderModel
+    {
+        public int Column { get; set; }
+        public string Dir { get; set; }
+    }
+
+    [ModelBinder(typeof(DataTablesParametersModelBinder))]
+    public class DataTableRecieverModel
+    {
+        public List<DataTablesColumnsModel> Columns { get; set; }
+        public int Draw { get; set; }
+        public int Length { get; set; }
+        public List<DataTablesOrderModel> Order { get; set; }
+        public DataTablesSearchModel Search { get; set; }
+        public int Start { get; set; }
+    }
+
+    public class DataTablesParametersModelBinder : DefaultModelBinder
+    {
+        public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            base.BindModel(controllerContext, bindingContext);
+            var request = controllerContext.HttpContext.Request;
+            // Retrieve request data
+            var draw = Convert.ToInt32(request["draw"]);
+            var start = Convert.ToInt32(request["start"]);
+            var length = Convert.ToInt32(request["length"]);
+            // Search
+            var search = new DataTablesSearchModel()
+            {
+                Value = request["search[value]"],
+                Regex = Convert.ToBoolean(request["search[regex]"])
+            };
+            // Order
+            var o = 0;
+            var order = new List<DataTablesOrderModel>();
+            while (request["order[" + o + "][column]"] != null)
+            {
+                order.Add(new DataTablesOrderModel()
+                {
+                    Column = Convert.ToInt32(request["order[" + o + "][column]"]),
+                    Dir = request["order[" + o + "][dir]"]
+                });
+                o++;
+            }
+            // Columns
+            var c = 0;
+            var columns = new List<DataTablesColumnsModel>();
+            while (request["columns[" + c + "][name]"] != null)
+            {
+                columns.Add(new DataTablesColumnsModel()
+                {
+                    Data = request["columns[" + c + "][data]"],
+                    Name = request["columns[" + c + "][name]"],
+                    Orderable = Convert.ToBoolean(request["columns[" + c + "][orderable]"]),
+                    Searchable = Convert.ToBoolean(request["columns[" + c + "][searchable]"]),
+                    Search = new DataTablesSearchModel()
+                    {
+                        Value = request["columns[" + c + "][search][value]"],
+                        Regex = Convert.ToBoolean(request["columns[" + c + "][search][regex]"])
+                    }
+                });
+                c++;
+            }
+
+            return new DataTableRecieverModel()
+            {
+                Draw = draw,
+                Start = start,
+                Length = length,
+                Search = search,
+                Order = order,
+                Columns = columns
+            };
+        }
+    }
+
+    public sealed class DataTablesSearchModel
+    {
+        public bool Regex { get; set; }
+        public string Value { get; set; }
+    }
+    
+}
--- Console/BExIS.Web.Shell/Controllers/UiTestController.cs
+++ Console/BExIS.Web.Shell/Controllers/UiTestController.cs
@@ -1,4 +1,10 @@
-﻿using BExIS.UI.Models;
+﻿using BExIS.Security.Entities.Subjects;
+using BExIS.Security.Services.Subjects;
+using BExIS.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
 using System.Web.Mvc;
 
 
@@ -10,6 +16,8 @@ namespace BExIS.Web.Shell.Controllers
         {
             return View();
         }
+
+        #region TreeView
 
         [HttpGet]
         public JsonResult TreeviewData()
@@ -67,6 +75,83 @@ namespace BExIS.Web.Shell.Controllers
 
             return Json(true, JsonRequestBehavior.AllowGet);
         }
-        
+
+        #endregion
+
+        #region datatables
+        [HttpPost]
+        public ActionResult Load(DataTableRecieverModel model)
+        {
+            using (UserManager userManager = new UserManager())
+            {
+                try
+                {
+                    int skip = model.Start;
+
+                    IQueryable<User> data = null;
+
+                    data = userManager.Users.AsQueryable();
+
+                    //search
+                    if (!string.IsNullOrEmpty(model.Search.Value))
+                    {
+                        string searchValue = model.Search.Value.ToLower();
+                        data = data.Where(i =>
+                            (i.Name != null && i.Name.ToLower().Contains(searchValue)) ||
+                            (i.DisplayName != null && i.DisplayName.ToLower().Contains(searchValue)) ||
+                            (i.Email != null && i.Email.ToLower().Contains(searchValue)));
+                    }
+
+                    int filteredRows = data.ToList().Count();
+
+                    //order by
+                    var sorting = string.Join(",", model.Order.Select(o => model.Columns[o.Column].Data + " " + o.Dir));
+                    if (!string.IsNullOrEmpty(sorting))
+                    {
+                        data = data.OrderBy(sorting);
+                    }
+
+                    //paging
+                    data = data.Skip(skip).Take(model.Length);
+
+                    int countAll = userManager.Users.Count();
+
+                    List<UserModel> tmp = new List<UserModel>();
+                    data.ToList().ForEach(u=> tmp.Add(new UserModel(u)));
+
+                    DataTableSendModel sendModel = new DataTableSendModel();
+                    sendModel.data = tmp;
+                    sendModel.draw = model.Draw;
+                    sendModel.recordsTotal = countAll;
+                    sendModel.recordsFiltered = filteredRows;
+
+                    return Json(sendModel);
+                }
+                catch (Exception exception)
+                {
+                    string json = "{\"error\":\"" + exception.Message + "\"}";
+
+                    return Json(json);
+                }
+            }
+        }
+
+        #endregion
+
+    }
+
+    public class UserModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        public UserModel(User user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            Email = user.Email;
+        }
     }
 }
