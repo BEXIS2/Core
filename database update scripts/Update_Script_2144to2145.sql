3fab2b0de11a05ef9d39d921ed153158d89ee98e
David Schöne <david.blaa@googlemail.com>
2021-10-04 09:06:40 +0200

    BEXIS2/Core#799 set ddm/data to public - db update script

--- database update scripts/Update_Script_2144to2145.sql
+++ database update scripts/Update_Script_2144to2145.sql
@@ -1,7 +1,18 @@
 BEGIN TRANSACTION;  
 
 -- ROLLBACK TRANSACTION;
+
+-- change datatype of Content descriptor uri field to text
 alter table ContentDescriptors alter column URI TYPE text;
+
+-- set ShowMultimediaData to public
+UPDATE public.operations
+	SET featureref=null
+	Where module = 'MMM' and controller = 'ShowMultimediaData';
+
+UPDATE public.operations
+	SET featureref=null
+	Where module = 'DDM' and controller = 'Data';
 
 -- Insert Data
 INSERT INTO public.versions(
