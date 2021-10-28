874a06f0b518c352f1dc95447862576d184b4015
David Schöne <david.blaa@googlemail.com>
2021-10-28 14:11:47 +0200

    BEXIS2/Core#835 add metadata edit hook and update the manifestfile

---
+++ Console/BExIS.Web.Shell/Areas/DCM/Hooks/MetadataEditHook.cs
@@ -0,0 +1,24 @@
+﻿using BExIS.UI.Hooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.Dcm.UI.Hooks
+{
+    public class MetadataEditHook : Hook
+    {
+        public MetadataEditHook()
+        { 
+
+        }
+
+        public override void Check()
+        {
+            // check status
+            // set entrypoint
+
+            throw new NotImplementedException();
+        }
+    }
+}
