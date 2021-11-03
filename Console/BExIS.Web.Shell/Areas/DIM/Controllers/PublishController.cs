66d73df1306ae6620cefb77bf65b85e476251197
David Schöne <david.blaa@googlemail.com>
2021-11-03 07:25:57 +0100

    BEXIS2/Core#839 add publishhook with controller and seeddata

---
+++ Console/BExIS.Web.Shell/Areas/DIM/Controllers/PublishController.cs
@@ -0,0 +1,32 @@
+﻿using BExIS.App.Bootstrap.Attributes;
+using BExIS.Dlm.Entities.Data;
+using BExIS.Security.Entities.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.Dim.UI.Controllers
+{
+    public class PublishController : Controller
+    {
+        // GET: Publish
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Entrypoint for Publish Hook
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="version"></param>
+        /// <returns></returns>
+        [BExISEntityAuthorize(typeof(Dataset), "id", RightType.Read)]
+        public ActionResult Start(long id, int version = 0)
+        {
+            return RedirectToAction("getPublishDataPartialView", "Submission", new { datasetId = id, versionId = version });
+        }
+    }
+}
---
+++ Console/BExIS.Web.Shell/Areas/DIM/Hooks/PublishHook.cs
@@ -0,0 +1,37 @@
+﻿using BExIS.Security.Entities.Authorization;
+using BExIS.UI.Hooks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.Dim.UI.Hooks
+{
+    public class PublishHook : Hook
+    {
+        public PublishHook()
+        {
+            Start = "dim/publish/start";
+        }
+
+        public override void Check(long id, string username)
+        {
+            // check status
+            checkStatus(id, username);
+
+        }
+
+        private void checkStatus(long id, string username)
+        {
+            // check if the user has access rights to the entrypoint - set in Start
+            bool hasAccess = hasUserAccessRights(username);
+
+            // user rights to the dataset
+            bool hasRights = hasUserEntityRights(id, username,RightType.Write);
+
+            // if one fail then access is denied
+            if(hasAccess == false || hasRights == false) Status = HookStatus.AccessDenied;
+        }
+
+    }
+}
