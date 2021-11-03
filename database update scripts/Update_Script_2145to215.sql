f4c714d4e4f1f2bdeb83fa5a42df4159dc7167f1
David Schöne <david.blaa@googlemail.com>
2021-11-03 07:34:29 +0100

    BEXIS2/Core#829 add notes to update script

---
+++ database update scripts/Update_Script_2145to215.sql
@@ -0,0 +1,17 @@
+BEGIN TRANSACTION;  
+
+-- ROLLBACK TRANSACTION;
+
+
+-- Add Controller to dcm --
+
+-- dcm/Metadata
+-- dcm/Validation
+-- dcm/FileUpload
+-- dcm/Edit
+-- ddm/view
+-- dim/publish 
+-- sam/userpermissions
+
+
+COMMIT;
