a0a44fc604a96ee68940c2a327669d21c679a10b
Franzi <franzi82@gmx.de>
2021-12-13 14:58:02 +0100

    Fix View party: conditional attribute not shown #725

--- Console/BExIS.Web.Shell/Areas/BAM/Scripts/PartyHelper.js
+++ Console/BExIS.Web.Shell/Areas/BAM/Scripts/PartyHelper.js
@@ -6,13 +6,18 @@
     var m;
     do {
         m = re.exec(condition);
+        console.log(m);
         if (m) {
             var element = $("[AttributeName='" + m[1].toLowerCase() + "']");
+            var element2 = $("[AttributeName='" + m[1].toLowerCase() + "']").find('span').html()
             elementVal = element.val();
             //TODO: more than one element is radio list but it might be different in future
             if (element.length > 1)
                 elementVal = $("[AttributeName='" + m[1].toLowerCase() + "']:checked").val();
-            newCondition = newCondition.replace(m[0], "'" + elementVal + "'");
+            else if (element2.length > 1)
+                elementVal = element2;
+
+            newCondition = newCondition.replace(m[0], "'" + element2 + "'");
         }
     } while (m)
     return newCondition;
