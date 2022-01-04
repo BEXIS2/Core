4b9d75fbd9976e31b13238267f061bd37b254684
Franzi <franzi82@gmx.de>
2022-01-04 18:41:04 +0100

    Update update script: add filesize #846

--- database update scripts/Update_Script_2145to2146.sql
+++ database update scripts/Update_Script_2145to2146.sql
@@ -2,3 +2,12 @@
 -- Add new description field and copy description from XML field (extra) to the new description column
 Alter Table contentdescriptors add column description text;
 Update contentdescriptors set description =  (xpath('/extra/fileDescription/text()', extra))[1]::text
+
+-- Add field for filz size
+Alter Table contentdescriptors add column filesize integer;
+
+-- Add new columns for Versions
+Alter Table datasetversions add column versiontype varchar(255);
+Alter Table datasetversions add column versionname varchar(255);
+Alter Table datasetversions add column versiondescription text;
+Alter Table datasetversions add column publicaccess boolean;
