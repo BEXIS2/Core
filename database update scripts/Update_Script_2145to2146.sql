fdf6277852bbcd2e9cc136640fcb4720b89f55d9
Franzi <franzi82@gmx.de>
2022-01-06 20:12:54 +0100

    Requests: Split feature permission into send and manage requests: Add update script #872

--- database update scripts/Update_Script_2145to2146.sql
+++ database update scripts/Update_Script_2145to2146.sql
@@ -11,3 +11,21 @@ Alter Table datasetversions add column versiontype varchar(255);
 Alter Table datasetversions add column versionname varchar(255);
 Alter Table datasetversions add column versiondescription text;
 Alter Table datasetversions add column publicaccess boolean;
+
+-- Change/Add splitted requests
+INSERT INTO public.features(
+	versionno, extra, description, name, parentref)
+	VALUES (1, null, 'Allow to send requests', 'Requests Send', (Select id from features where name = 'Data Discovery'));
+
+INSERT INTO public.operations
+(versionno, extra, "module", controller, "action", featureref)
+VALUES(1, NULL, 'DDM', 'RequestsSend', '*', (
+Select id from features where name = 'Requests Send' AND parentref = (
+Select id from features where name = 'Data Discovery')));
+
+Update features set name = 'Requests Manage' where name = 'Requests';
+Update features set description = 'Manange requests by user' where name = 'Requests Manage';
+
+update operations set controller = 'RequestsManage' where controller = 'Requests';
+
+-- Set old permission settings from requests manange to send as well? 
