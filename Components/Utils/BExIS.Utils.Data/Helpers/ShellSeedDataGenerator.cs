1533bfd93ee0baa5cf80130938deb428814ae2bd
David Schöne <david.blaa@googlemail.com>
2022-01-10 14:17:50 +0100

    BEXIS2/Core#818 add settings controller to seed data

--- Components/Utils/BExIS.Utils.Data/Helpers/ShellSeedDataGenerator.cs
+++ Components/Utils/BExIS.Utils.Data/Helpers/ShellSeedDataGenerator.cs
@@ -27,11 +27,10 @@ namespace BExIS.Utils.Data.Helpers
                 var o7 = operationManager.Find("Shell", "Ldap", "*") ?? operationManager.Create("Shell", "Ldap", "*");
                 var o8 = operationManager.Find("Shell", "Help", "*") ?? operationManager.Create("Shell", "Help", "*");
 
-                var o11 = operationManager.Find("Shell", "Settings", "*") ?? operationManager.Create("Shell", "Settings", "*", settings);
-
-                if (!versionManager.Exists("Shell", "2.14.3"))
                 var o9 = operationManager.Find("Shell", "UiTest", "*") ?? operationManager.Create("Shell", "Help", "*");
                 var o10 = operationManager.Find("Shell", "Menu", "*") ?? operationManager.Create("Shell", "Help", "*");
+
+                var o11 = operationManager.Find("Shell", "Settings", "*") ?? operationManager.Create("Shell", "Settings", "*", settings);
 
                 if (!versionManager.Exists("Shell", "2.14.5"))
                 {
