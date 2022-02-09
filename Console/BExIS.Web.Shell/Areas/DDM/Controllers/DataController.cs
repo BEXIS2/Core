77beda69dd06e7588abe3f9d5d803ebd42dcc6cc
Franzi <franzi82@gmx.de>
2022-02-09 14:17:37 +0100

    Primary Data View: Reduce default number of shown rows from 100 to 10; set default in code #886

--- Console/BExIS.Web.Shell/Areas/DDM/Controllers/DataController.cs
+++ Console/BExIS.Web.Shell/Areas/DDM/Controllers/DataController.cs
@@ -599,7 +599,7 @@ namespace BExIS.Modules.Ddm.UI.Controllers
                             try
                             {
                                 long count = dm.RowCount(datasetID, null);
-                                if (count > 0) table = dm.GetLatestDatasetVersionTuples(datasetID, null, null, null, 0, 100);
+                                if (count > 0) table = dm.GetLatestDatasetVersionTuples(datasetID, null, null, null, 0, 10);
                                 else ModelState.AddModelError(string.Empty, "<span style=\"color: black;\"> There is no primary data available/uploaded. </span><br/><br/> <span style=\"font-weight: normal;color: black;\">Please note that the data may have been uploaded to another repository and is referenced here in the metadata.</span>");
                             }
                             catch
@@ -611,7 +611,7 @@ namespace BExIS.Modules.Ddm.UI.Controllers
                         }
                         else
                         {
-                            table = dm.GetDatasetVersionTuples(versionId, 0, 100);
+                            table = dm.GetDatasetVersionTuples(versionId, 0, 10);
                             Session["gridTotal"] = dm.GetDatasetVersionEffectiveTuples(dsv).Count;
                         }
                         
