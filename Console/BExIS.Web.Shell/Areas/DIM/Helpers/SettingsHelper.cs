8ebf3eec7eb0e27e35ea6765a2bbf7377499b7c8
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-05-24 11:38:10 +0200

    #905 update dim and include/extend settings file

---
+++ Console/BExIS.Web.Shell/Areas/DIM/Helpers/SettingsHelper.cs
@@ -0,0 +1,40 @@
+﻿using BExIS.Xml.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using Vaiona.Utils.Cfg;
+
+namespace BExIS.Modules.Dim.UI.Helpers
+{
+    public class SettingsHelper
+    {
+        string filePath = "";
+
+        public SettingsHelper()
+        {
+            filePath = Path.Combine(AppConfiguration.GetModuleWorkspacePath("DIM"), "Dim.Settings.xml");
+        }
+
+        public bool KeyExist(string key)
+        {
+            XDocument settings = XDocument.Load(filePath);
+            XElement element = XmlUtility.GetXElementByAttribute("entry", "key", key.ToLower(), settings);
+
+            return element != null ? true : false;
+        }
+
+        public string GetValue(string key)
+        {
+            XDocument settings = XDocument.Load(filePath);
+            XElement element = XmlUtility.GetXElementByAttribute("entry", "key", key.ToLower(), settings);
+
+            string value = "";
+            value = element.Attribute("value")?.Value;
+
+            return value;
+        }
+    }
+}
