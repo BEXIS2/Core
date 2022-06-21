ea53f65e4213d3059721549aaf3b503a3dcd7f4a
Franzi <franzi82@gmx.de>
2022-06-21 16:32:57 +0200

    Add settings to control internal latest version and content of version select list #853

---
+++ Components/AAA/BExIS.Security.Entities/FormerMember/EntityPermissionFormerMember.cs
@@ -0,0 +1,19 @@
+﻿using BExIS.Security.Entities.Objects;
+using BExIS.Security.Entities.Subjects;
+using Vaiona.Entities.Common;
+
+namespace BExIS.Security.Entities.FormerMember
+{
+
+
+    /// <summary>
+    ///
+    /// </summary>
+    public class EntityPermissionFormerMember : BaseEntity
+    {
+        public virtual Entity Entity { get; set; }
+        public virtual long Key { get; set; }
+        public virtual int Rights { get; set; }
+        public virtual Subject Subject { get; set; }
+    }
+}
---
+++ Components/AAA/BExIS.Security.Services/FormerMember/FormerMemberUsersGroupsRelationManager.cs
@@ -0,0 +1,76 @@
+﻿using BExIS.Security.Entities.FormerMember;
+using System;
+using System.Linq;
+using Vaiona.Persistence.Api;
+
+namespace BExIS.Security.Services.FormerMember
+{
+    public class FormerMemberUsersGroupsRelationManager : IDisposable
+    {
+        private readonly IUnitOfWork _guow;
+        private bool _isDisposed;
+
+        public IReadOnlyRepository<UsersGroupsRelationFormerMember> FormerMemberUsersGroupsRelationRepository { get; }
+
+        public IQueryable<UsersGroupsRelationFormerMember> FormerMemberFeaturePermissions => FormerMemberUsersGroupsRelationRepository.Query();
+
+        public FormerMemberUsersGroupsRelationManager()
+        {
+            _guow = this.GetIsolatedUnitOfWork();
+            FormerMemberUsersGroupsRelationRepository = _guow.GetReadOnlyRepository<UsersGroupsRelationFormerMember>();
+        }
+
+        ~FormerMemberUsersGroupsRelationManager()
+        {
+            Dispose(true);
+        }
+
+
+        public void Create(long userRefId, long groupRefId)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                
+                var formerMemberUsersGroupsRelation = new UsersGroupsRelationFormerMember()
+                {
+                    UserRef = userRefId,
+                    GroupRef = groupRefId
+                };
+
+                var formerMemberUsersGroupsRelationRepository = uow.GetRepository<UsersGroupsRelationFormerMember>();
+                formerMemberUsersGroupsRelationRepository.Put(formerMemberUsersGroupsRelation);
+                uow.Commit();
+            }
+        }
+
+        public void Delete(UsersGroupsRelationFormerMember formerMemberUsersGroupsRelation)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var formerMemberUsersGroupsRelationRepository = uow.GetRepository<UsersGroupsRelationFormerMember>();
+                formerMemberUsersGroupsRelationRepository.Delete(formerMemberUsersGroupsRelation);
+                uow.Commit();
+            }
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    if (_guow != null)
+                        _guow.Dispose();
+                    _isDisposed = true;
+                }
+            }
+        }
+    }
+}
