847ab6b3122ddfff0b9415116c2d7afcb5401bd9
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-06-22 09:30:30 +0200

    #905 tests for ui related classes of doi creation

--- Modules/DIM/BExIS.Modules.Dim.UI.Tests/BExIS.Modules.Dim.UI.Tests/Helpers/SettingsHelperTests.cs
+++ Modules/DIM/BExIS.Modules.Dim.UI.Tests/BExIS.Modules.Dim.UI.Tests/Helpers/SettingsHelperTests.cs
@@ -5,6 +5,12 @@ using BExIS.Utils.Config;
 using Vaiona.Utils.Cfg;
 using BExIS.Dim.Helpers.Models;
 using System.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using BExIS.Dim.Helpers;
+using BExIS.Dlm.Entities.Data;
+using Lucifron.ReST.Library.Models;
+using Newtonsoft.Json;
 
 namespace BExIS.Modules.Dim.UI.Tests.Helpers
 {
@@ -25,6 +31,18 @@ namespace BExIS.Modules.Dim.UI.Tests.Helpers
         }
 
         [Test()]
+        public void ReadProxy_ValidSettings_ReturnString()
+        {
+            var appConfiguration = AppConfiguration.WorkspaceRootPath;
+            var settingsHelper = new SettingsHelper();
+
+
+            var proxy = settingsHelper.GetValue("proxy");
+
+            Assert.That(1, Is.EqualTo(1));
+        }
+
+        [Test()]
         public void ReadMappings_ValidSettings_ReturnList()
         {
             var appConfiguration = AppConfiguration.WorkspaceRootPath;
@@ -42,11 +60,29 @@ namespace BExIS.Modules.Dim.UI.Tests.Helpers
         {
             var appConfiguration = AppConfiguration.WorkspaceRootPath;
             var settingsHelper = new SettingsHelper();
+            var datacitedoihelper = new DataCiteDoiHelper();
 
 
+            var client = new RestClient(settingsHelper.GetValue("proxy"));
+            client.Authenticator = new JwtAuthenticator(settingsHelper.GetValue("token"));
+
+            // DOI
             var placeholders = settingsHelper.GetDataCiteSettings("placeholders");
 
-            Assert.That(placeholders.Count, Is.EqualTo(1));
+            var x = new DatasetVersion() { Id = 1337, VersionName = "abc", VersionNo = 111, Dataset = new Dataset() { Id = 42} };
+
+            var p = datacitedoihelper.CreatePlaceholders(x, placeholders);
+
+            var doi_request = new RestRequest($"api/dois", Method.POST).AddJsonBody(p);
+            CreateDOIModel doi = JsonConvert.DeserializeObject<CreateDOIModel>(client.Execute(doi_request).Content);
+
+            var model = new CreateDataCiteModel();
+            model.Doi = doi.DOI;
+            model.Suffix = doi.Suffix;
+            model.Prefix = doi.Prefix;
+
+            var datacite_request = new RestRequest($"api/datacite", Method.POST).AddJsonBody(JsonConvert.SerializeObject(model));
+            var response = client.Execute(datacite_request).Content;
         }
     }
 }
