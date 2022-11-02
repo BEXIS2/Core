389e2979307ee56b11cb6db484bff4d0a363be72
Martin Hohmuth <martin.hohmuth@uni-jena.de>
2022-11-02 16:46:14 +0100

    #847 restructuring RPM

---
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI/IO/VariableTemplateIO.cs
@@ -0,0 +1,119 @@
+﻿using BExIS.Dlm.Entities.DataStructure;
+using BExIS.Dlm.Services.DataStructure;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+
+namespace BExIS.Modules.Rpm.UI.Classes
+{
+    public static class VariableTemplateIO
+    {
+        public static VariableTemplate getVariableTemplate(long variableTemplateId)
+        {
+            if (variableTemplateId > 0)
+            {
+                VariableManager variableManager = null;
+                try
+                {
+                    variableManager = new VariableManager();
+                    return variableManager.GetVariableTemplate(variableTemplateId);
+                }
+                finally
+                {
+                    variableManager.Dispose();
+                }
+            }
+            return null;
+        }
+        public static List<VariableTemplate> getVariableTemplates()
+        {
+            VariableManager variableManager = null;
+            try
+            {
+                variableManager = new VariableManager();
+                return variableManager.VariableTemplateRepo.Get().ToList();
+            }
+            finally
+            {
+                variableManager.Dispose();
+            }
+        }
+        public static VariableTemplate createVariableTemplate(VariableTemplate variableTemplate)
+        {
+            if (variableTemplate != null && variableTemplate.Id == 0)
+            {
+                VariableManager variableManager = null;
+                try
+                {
+                    variableManager = new VariableManager();
+                    return variableManager.CreateVariableTemplate(variableTemplate.Label, variableTemplate.DataType, variableTemplate.Unit, variableTemplate.IsValueOptional, variableTemplate.Description = "", variableTemplate.DefaultValue, variableTemplate.DisplayPatternId);
+                }
+                finally
+                {
+                    variableManager.Dispose();
+                }
+            }
+            return null;
+        }
+        public static bool deleteVariableTemplate(VariableTemplate variableTemplate)
+        {
+            if (variableTemplate != null)
+            {
+                VariableManager variableManager = null;
+                try
+                {
+                    variableManager = new VariableManager();
+                    return variableManager.DeleteVariableTemplate(variableTemplate.Id);
+                }
+                finally
+                {
+                    variableManager.Dispose();
+                }
+            }
+            return false;
+        }
+        public static VariableTemplate editVariableTemplate(VariableTemplate variableTemplate)
+        {
+            if (variableTemplate != null && variableTemplate.Id != 0)
+            {
+                VariableManager variableManager = null;
+                try
+                {
+                    variableManager = new VariableManager();
+                    return variableManager.UpdateVariableTemplate(variableTemplate);
+                }
+                finally
+                {
+                    variableManager.Dispose();
+                }
+            }
+            return null;
+        }
+        public static string variableTemplateToJson(VariableTemplate variableTemplate)
+        {
+            if (variableTemplate != null)
+            {
+                return JsonConvert.SerializeObject(variableTemplate);
+            }
+            return null;
+        }
+        public static VariableTemplate variableTemplateToJson(string json)
+        {
+            if (json != null && json.Length > 0)
+            {
+                return JsonConvert.DeserializeObject<VariableTemplate>(json);
+            }
+            return null;
+        }
+        public static string variableTemplatesToJson(List<VariableTemplate> variableTemplates)
+        {
+            if (variableTemplates != null && variableTemplates.Count() > 0)
+            {
+                return JsonConvert.SerializeObject(variableTemplates);
+            }
+            return null;
+        }
+
+    }
+}
