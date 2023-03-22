09592d13eab75674e2f05efaa5a3548758d4a49a
Sven Thiel <m6thsv2@googlemail.com>
2023-03-22 07:58:38 +0100

    #938 add first models for DataCiteDOI communication

---
+++ Console/BExIS.Web.Shell/Areas/DIM/Models/DataCiteDoiModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vaelastrasz.Library.Models;
+
+namespace BExIS.Modules.Dim.UI.Models
+{
+    public class CreateDataCiteDoiModel
+    {
+        public long DatasetId { get; set; }
+        public long DatasetVersionId { get; set; }
+        public CreateDataCiteModel DataCiteModel { get; set; }
+
+        public CreateDataCiteDoiModel(long datasetId, long datasetVersionId)
+        {
+            DatasetId = datasetId;
+            DatasetVersionId = datasetVersionId;
+            DataCiteModel = new CreateDataCiteModel();
+        }
+
+        public CreateDataCiteDoiModel(long datasetId, long datasetVersionId, CreateDataCiteModel dataCiteModel)
+        {
+            DatasetId = datasetId;
+            DatasetVersionId = datasetVersionId;
+            DataCiteModel = dataCiteModel;
+        }
+    }
+
+    public class ReadDataCiteDoiModel
+    {
+
+    }
+
+    public class UpdateDataCiteDoiModel
+    {
+
+    }
+}
