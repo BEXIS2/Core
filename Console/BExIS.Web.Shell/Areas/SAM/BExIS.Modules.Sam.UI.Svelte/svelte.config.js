42738e660cfddf10e77f5166b6554d3d43754e91
Sven Thiel <m6thsv2@googlemail.com>
2023-06-07 10:23:45 +0200

    #997 update project structure to be on the latest state

--- Console/BExIS.Web.Shell/Areas/SAM/BExIS.Modules.Sam.UI.Svelte/svelte.config.js
+++ Console/BExIS.Web.Shell/Areas/SAM/BExIS.Modules.Sam.UI.Svelte/svelte.config.js
@@ -13,14 +13,21 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages:'', // ../BExIS.Modules.Dcm.UI/Scripts/svelte
-			assets:"",// ../BExIS.Modules.Dcm.UI/Scripts/svelte
+			pages:'../BExIS.Modules.Sam.UI/Scripts/svelte', // ../BExIS.Modules.Dcm.UI/Scripts/svelte
+			assets:'../BExIS.Modules.Sam.UI/Scripts/svelte',// ../BExIS.Modules.Dcm.UI/Scripts/svelte
 			fallback:null,
 			precompress:true,
 			preprocess:true,
 			strict:false
 		}
-		)
+		),
+		paths:{
+			base: process.env.NODE_ENV ==='production' ? '/sam':'' // add module id here e.g. /dcm
+		},
+		alias: {
+			$models: './src/models',
+			$services: './src/services',
+	}
 
 	}
 };
