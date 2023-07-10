883bdc4fae57d1ae929f87cba0c50879100ab7fa
David Schöne <david.blaa@googlemail.com>
2023-07-10 14:02:36 +0200

    #1234 chnage base path insvelte config

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/svelte.config.js
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			strict:false
 		}),
 		paths:{
-			base: process.env.NODE_ENV ==='production' ? '/dcm':'' // add module id here
+			base: process.env.NODE_ENV ==='production' ? '/areas/dcm/Scripts/svelte':'' // add module id here
 		},
 
 		alias: {
