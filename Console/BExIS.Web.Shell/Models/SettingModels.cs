c2e6c6ab3e42f29a01e957806351a807e130d001
Sven Thiel <m6thsv2@googlemail.com>
2023-07-20 10:09:27 +0200

    #1163 renaming of models and references

---
+++ Console/BExIS.Web.Shell/Models/SettingModels.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vaiona.Utils.Cfg;
+
+namespace BExIS.Web.Shell.Models
+{
+    public class ReadSettingModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("entries")]
+        public List<Entry> Entries { get; set; }
+
+        public static ReadSettingModel Convert(JsonSettings settings)
+        {
+            return new ReadSettingModel()
+            {
+                Id = settings.Id,
+                Name = settings.Name,
+                Description = settings.Description,
+                Entries = settings.Entries
+            };
+        }
+    }
+
+    public class UpdateSettingModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("entries")]
+        public List<Entry> Entries { get; set; }
+
+        public static UpdateSettingModel Convert(JsonSettings settings)
+        {
+            return new UpdateSettingModel()
+            {
+                Id= settings.Id,
+                Name = settings.Name,
+                Description = settings.Description,
+                Entries = settings.Entries
+            };
+        }
+    }
+
+    public class ModulModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
