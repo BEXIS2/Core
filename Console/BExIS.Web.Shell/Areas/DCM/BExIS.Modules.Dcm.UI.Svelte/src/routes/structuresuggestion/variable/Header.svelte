533d78b6a921a0c9f67a350cb35c371e200bf717
David Schöne <david.blaa@googlemail.com>
2023-07-25 10:56:07 +0200

    #1257 display validation result with success

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/structuresuggestion/variable/Header.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/structuresuggestion/variable/Header.svelte
@@ -2,17 +2,24 @@
 
  import {SlideToggle} from '@skeletonlabs/skeleton'
  import {Alert} from '@bexis2/bexis2-core-ui'
+ import Fa from 'svelte-fa';
+ import {faCheck} from '@fortawesome/free-solid-svg-icons'
+	import Variable from './Variable.svelte';
 
  export let index;
  export let name;
  export let isKey;
  export let isOptional;
+ export let isValid;
 
  </script>
 
-<div class="flex gap-5">
+<div id={name} class="flex gap-5">
 
-    <div class="grow"><h1 class="h1">{name}</h1></div>
+    <div class="grow flex gap-2">
+      <h1 class="h1">{name}</h1> 
+      <div class="text-success-500">{#if isValid}<Fa icon={faCheck}/>{/if}</div>
+    </div>
 
     <div  class="flex-none max-w-5xl flex gap-2 text-right">
     {#if isKey && isOptional}
@@ -35,5 +42,4 @@
       <div><SlideToggle size="sm" name="isKey" bind:checked={isKey} active="bg-primary-500">Mark as part of primary keys</SlideToggle></div>
       <div><SlideToggle size="sm" name="isOptional" active="bg-primary-500" bind:checked={isOptional}>Value can be optional</SlideToggle></div>
     </div>
-
 </div>
