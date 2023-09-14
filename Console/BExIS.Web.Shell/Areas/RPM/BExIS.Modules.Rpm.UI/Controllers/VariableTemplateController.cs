464cd78c9deebe040c897019590471f6f0490174
David Schöne <david.blaa@googlemail.com>
2023-09-14 14:37:27 +0200

    #849 add Varaible Template Controller

---
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI/Controllers/VariableTemplateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BExIS.Modules.Rpm.UI.Controllers
+{
+    public class VariableTemplateController : Controller
+    {
+        // GET: VariableTemplate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
