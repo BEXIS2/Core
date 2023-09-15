4af180ab16cf627779662dea3ad81b26e1dd917a
Sven Thiel <m6thsv2@googlemail.com>
2023-09-15 10:31:51 +0200

    #1276 add class for jwt settings

--- Components/Utils/BExIS.Utils.Config/Configurations/JwtConfiguration.cs
+++ Components/Utils/BExIS.Utils.Config/Configurations/JwtConfiguration.cs
@@ -11,25 +11,25 @@ namespace BExIS.Utils.Config.Configurations
     {
         [JsonProperty("issuerSigningKey")]
         public string IssuerSigningKey { get; set; }
-        
+
         [JsonProperty("requireExpirationTime")]
         public bool RequireExpirationTime { get; set; }
-        
+
         [JsonProperty("validateAudience")]
         public bool ValidateAudience { get; set; }
-        
+
         [JsonProperty("validateIssuer")]
         public bool ValidateIssuer { get; set; }
-        
+
         [JsonProperty("validateLifetime")]
         public bool ValidateLifetime { get; set; }
-        
+
         [JsonProperty("validAudience")]
         public string ValidAudience { get; set; }
-        
+
         [JsonProperty("validIssuer")]
         public string ValidIssuer { get; set; }
-        
+
         [JsonProperty("validLifetime")]
         public int ValidLifetime { get; set; }
     }
