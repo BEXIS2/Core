7f9e5052a4831c7688c1d9c0e78312c15e3763cf
Sven Thiel <m6thsv2@googlemail.com>
2023-10-02 09:16:27 +0200

    #1330 add api controller

---
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI/Controllers/API/ConstraintsController.cs
@@ -0,0 +1,94 @@
+﻿using BExIS.Dlm.Services.DataStructure;
+using BExIS.Modules.Rpm.UI.Models;
+using BExIS.Security.Services.Subjects;
+using BExIS.Utils.Route;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Vaiona.Web.Mvc.Modularity;
+
+namespace BExIS.Modules.Rpm.UI.Controllers.API
+{
+    public class ConstraintsController : ApiController
+    {
+        [HttpGet, GetRoute("api/constraints")]
+        public async Task<HttpResponseMessage> GetConstraints()
+        {
+            try
+            {
+                using (var constraintManager = new ConstraintManager())
+                {
+                    var constraints = constraintManager.Repo.Query().ToList();
+                    var model = constraints.Select(u => ReadConstraintModel.Convert(u));
+
+                    return Request.CreateResponse(HttpStatusCode.OK, model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpGet, GetRoute("api/domainConstraints")]
+        public async Task<HttpResponseMessage> GetDomainConstraints()
+        {
+            try
+            {
+                using (var constraintManager = new ConstraintManager())
+                {
+                    var constraints = constraintManager.DomainConstraintRepo.Query().ToList();
+                    var model = constraints.Select(u => ReadDomainConstraintModel.Convert(u));
+
+                    return Request.CreateResponse(HttpStatusCode.OK, model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpGet, GetRoute("api/patternConstraints")]
+        public async Task<HttpResponseMessage> GetPatternConstraints()
+        {
+            try
+            {
+                using (var constraintManager = new ConstraintManager())
+                {
+                    var constraints = constraintManager.PatternConstraintRepo.Query().ToList();
+                    var model = constraints.Select(u => ReadPatternConstraintModel.Convert(u));
+
+                    return Request.CreateResponse(HttpStatusCode.OK, model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpGet, GetRoute("api/rangeConstraints")]
+        public async Task<HttpResponseMessage> GetRangeConstraints()
+        {
+            try
+            {
+                using (var constraintManager = new ConstraintManager())
+                {
+                    var constraints = constraintManager.RangeConstraintRepo.Query().ToList();
+                    var model = constraints.Select(u => ReadRangeConstraintModel.Convert(u));
+
+                    return Request.CreateResponse(HttpStatusCode.OK, model);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
