11df4d03c81df4b57bf4000349c411b6ef1ec62b
David Schöne <david.blaa@googlemail.com>
2023-10-13 11:15:39 +0200

    #848 check duplicate link and meanings

--- Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI.Svelte/src/routes/meanings/ExternalLink.svelte
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI.Svelte/src/routes/meanings/ExternalLink.svelte
@@ -2,34 +2,93 @@
 	import { TextInput } from "@bexis2/bexis2-core-ui";
 	import Fa from "svelte-fa";
 	import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
-	import { faPlus } from '@fortawesome/free-solid-svg-icons';
+	import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 
-export let name:string = "";
-export let type:string = "";
-export let uRI:string = "";
+	import suite from './externalLink';
+	import type { externalLinkType } from "./types";
+
+
+export let index:number;
+export let link:externalLinkType;
 export let last = true;
 
+export let isValid:boolean = false;
+// validation
+let res = suite.get();
+$: isValid = res.isValid();
+
+
 const dispatch = createEventDispatcher();
+
+
+onMount(() => {
+
+		// reset & reload validation
+		suite.reset();
+
+		// if link is allready stored, no need for validation
+		if(link.id > 0) 
+		{
+			isValid = true;
+		}
+
+	});
 
 function remove() {
 		dispatch('remove');
 	}
 
+	function onChangeFn(e)
+	{
+		if(link.id==0) // validate only new links with id == 0 
+		{
+			res = suite(link, e.target.id);
+			setValidationState(res);
+		}
+	}
+
+function setValidationState(res) {
+	
+		isValid = res.isValid();
+		console.log("🚀 ~ file: ExternalLink.svelte:55 ~ setValidationState ~ isValid:", isValid, index)
+		// dispatch this event to the parent to check the save button
+		dispatch('link-change');
+	}
 
 </script>
 
-<div class="flex space-x-3 items-center">
+<div id="{""+index}" class="flex space-x-3 items-center">
 
- <TextInput id="link_name" bind:value={name} on:change placeholder="Name"/>
+ <TextInput 
+		id="link_name" 
+		bind:value={link.name} 
+		on:change 
+		on:input={onChangeFn}
+		placeholder="Name"
+		valid={res.isValid('link_name')}
+		invalid={res.hasErrors('link_name')}
+		feedback={res.getErrors('link_name')}
+					/>
 
-	<TextInput id="link_type" bind:value={type} on:change placeholder="Type"/>
+	<TextInput id="link_type" bind:value={link.type} on:change placeholder="Type"/>
 
-	<TextInput id="link_uri" bind:value={uRI} on:change placeholder="Uri"/>
+	<TextInput 
+	id="link_uri" 
+	bind:value={link.uri} 
+	on:change 
+	on:input={onChangeFn}
+	placeholder="Uri"
+	valid={res.isValid('link_uri')}
+	invalid={res.hasErrors('link_uri')}
+	feedback={res.getErrors('link_uri')}
+	/>
 
-	<div class="self-center text-xl w-12">
+	<div class="flex text-xl w-12 gap-2">
 		<button title="delete" type="button" on:click={remove}><Fa icon={faTrashAlt} /></button>
-
+		{#if isValid}
+			<Fa class="text-success-500" icon="{faCheck}"/>
+		{/if}
 	</div>
 </div>
--- Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI.Svelte/src/routes/meanings/ExternslLinks.svelte
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI.Svelte/src/routes/meanings/ExternslLinks.svelte
@@ -10,18 +10,22 @@
 
 	import Fa from 'svelte-fa';
 
-	import { faAdd } from '@fortawesome/free-solid-svg-icons';
+	import { faAdd, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 	let selectableLinks: externalLinkType[] = [];
 	$: selectableLinks;
 
 	let selectedExternalLink: externalLinkType;
+	export let valid:boolean = false;
+	let linkValidationStates:any = [];
 
 	onMount(() => {
 		//set selectableLinks
 		updateSelectableLinks();
+		fillLinkValidationStates(list)
 
 		if (list.length === 0) {
+		
 			add();
 		}
 	});
@@ -32,6 +36,9 @@
 		if (list.length === 0) {
 			add();
 		}
+
+		updateSelectableLinks();
+		checkValidationState();
 	}
 
 	function add() {
@@ -49,45 +56,69 @@
 	}
 
 	function updateSelectableLinks() {
-		selectableLinks = $externalLinksStore.filter((e) => !list.includes(e));
+		selectableLinks = $externalLinksStore.filter((e) => !list.find(i => i.uri == e.uri));
+
 	}
 
 	function onSelectHandler(e) {
 		list = [...list, selectedExternalLink];
+
+		updateSelectableLinks()
 		selectedExternalLink = null;
 	}
+
+function fillLinkValidationStates(l) {
+
+linkValidationStates = [];
+
+for (let index = 0; index < l.length; index++) {
+	linkValidationStates.push(false);
+}
+}
+
+// every time when validation state of a varaible is change,
+// this function triggered an check wheter save button can be active or not
+function checkValidationState() {
+	
+valid = linkValidationStates.every((v) => v === true);
+console.log("🚀 ~ file: ExternslLinks.svelte:83 ~ checkValidationState ~ linkValidationStates:", linkValidationStates)
+//console.log("TCL ~ file: Variables.svelte:63 ~ checkValidationState ~ linkValidationStates:", linkValidationStates)
+}
 </script>
 
 
 
-<div class="link-container space-y-2">
+<div class="link-container space-y-4">
 
-	<label><b>External Links</b></label>
+	<label class="flex gap-3"><b>External Links</b>
+	{#if valid}<span class="text-success-500"><Fa icon="{faCheck}"/></span>{/if}
+	</label> 
 
 	{#if list}
-		{#each list as item, i}
+		{#each list as link, index}
 			<!-- content here -->
 			<Link
-				bind:name={item.name}
-				bind:type={item.type}
-				bind:uRI={item.uri}
-				on:add={add}
-				on:remove={() => remove(i)}
-				last={list.length - 1 === i}
+				{index}
+				{link}
+				
+				bind:isValid={linkValidationStates[index]}
+				on:remove={() => remove(index)}
+				on:link-change={checkValidationState}
+				last={list.length - 1 === index}
 			/>
 		{/each}
 	{/if}
 
 
 </div>
-<div class="flex items-center gap-3 w-3/4">
+<div class="flex items-center gap-3 w-1/2">
 	<div class="grow">
 		<MultiSelect
 			id="links"
 			bind:source={selectableLinks}
 			itemId="id"
 			itemLabel="name"
-			itemGroup="group"
+			itemGroup="type"
 			complexSource={true}
 			complexTarget={true}
 			bind:target={selectedExternalLink}
@@ -96,7 +127,7 @@
 			on:change={(e) => onSelectHandler(e)}
 		/>
 	</div>
-	<span class="span w-4 text-center">Or</span>
+	<span class="span w-14 text-center">or</span>
 	<div class="inline-block align-bottom">	
 		<button type="button" class="btn variant-filled-primary" on:click={add}><Fa icon={faAdd} /></button>
 </div>
