d0ccc7e66c606dc886a2280d9a8f1e117528fa3c
Sven Thiel <m6thsv2@googlemail.com>
2023-10-18 16:27:39 +0200

    #1274 update tests project for rpm ui

---
+++ Modules/RPM/BExIS.Modules.Rpm.UI.Tests/Controllers/API/ConstraintsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using BExIS.App.Testing;
+using BExIS.Modules.Rpm.UI.Controllers.API;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BExIS.Modules.Rpm.UI.Test.Controllers.API
+{
+    [TestFixture()]
+    public class ConstraintsControllerTests
+    {
+        private TestSetupHelper helper = null;
+
+        [Test()]
+        public void GetDataCiteDOICredentials()
+        {
+            //var appConfiguration = AppConfiguration.WorkspaceRootPath;
+            var constraintsController = new ConstraintsController();
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            //helper = new TestSetupHelper(WebApiConfig.Register, false);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+        }
+    }
+}
