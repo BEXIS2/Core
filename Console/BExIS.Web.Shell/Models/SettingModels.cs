69195061d2f9d58fa2d3af7fbbbf05b14456692d
Sven Thiel <m6thsv2@googlemail.com>
2023-10-23 17:13:53 +0200

    #1136 add new update model for settings

--- Console/BExIS.Web.Shell/Models/SettingModels.cs
+++ Console/BExIS.Web.Shell/Models/SettingModels.cs
@@ -7,19 +7,31 @@ using Vaiona.Utils.Cfg;
 
 namespace BExIS.Web.Shell.Models
 {
-    public class ReadSettingModel
+    public class ModulModel
     {
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
         [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonProperty("name")]
-        public string Name { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
 
+    public class ReadSettingModel
+    {
         [JsonProperty("description")]
         public string Description { get; set; }
 
         [JsonProperty("entries")]
         public List<Entry> Entries { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
         public static ReadSettingModel Convert(JsonSettings settings)
         {
@@ -35,39 +47,23 @@ namespace BExIS.Web.Shell.Models
 
     public class UpdateSettingModel
     {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
         [JsonProperty("description")]
         public string Description { get; set; }
 
         [JsonProperty("entries")]
         public List<Entry> Entries { get; set; }
 
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
         public static UpdateSettingModel Convert(JsonSettings settings)
         {
             return new UpdateSettingModel()
             {
-                Id= settings.Id,
                 Name = settings.Name,
                 Description = settings.Description,
                 Entries = settings.Entries
             };
         }
-    }
-
-    public class ModulModel
-    {
-        [JsonProperty("id")]
-        public string Id { get; set; }
-
-        [JsonProperty("title")]
-        public string Title { get; set; }
-
-        [JsonProperty("description")]
-        public string Description { get; set; }
     }
 }
