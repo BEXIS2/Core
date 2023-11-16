75ef2dd60d0c803b0c29ed5addc8f85ad1f82825
David Schöne <david.blaa@googlemail.com>
2023-11-16 08:00:51 +0100

    #1392 add init stores in layout files in shell, dcm

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/+layout.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 
 	//Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores();
+
 </script>
 
 <slot />
