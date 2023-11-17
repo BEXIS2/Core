3bf6552ac5363beb13a01f743a5ec26d9cd232dc
David Schöne <david.blaa@googlemail.com>
2023-11-17 08:54:19 +0100

    #1392 update theme in layout

---
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI/Helpers/MeaningHelper.cs
@@ -0,0 +1,42 @@
+﻿using BExIS.Dlm.Entities.Meanings;
+using BExIS.Modules.Rpm.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BExIS.Modules.Rpm.UI.Helpers
+{
+    public class MeaningsHelper
+    {
+        public static PrefixCategoryListItem ConvertTo(PrefixCategory prefixCategory)
+        { 
+            if (prefixCategory == null)
+                return null;
+            if (prefixCategory.Name == null)
+                throw new ArgumentNullException(nameof(prefixCategory));
+            
+            PrefixCategoryListItem prefixCategoryListItem = new PrefixCategoryListItem();
+            prefixCategoryListItem.Name = prefixCategory.Name;
+            prefixCategoryListItem.Description = prefixCategory.Description;
+            prefixCategoryListItem.Id = prefixCategory.Id;
+
+            return prefixCategoryListItem;
+        }
+
+        public static PrefixCategory ConvertTo(PrefixCategoryListItem prefixCategoryListItem)
+        {
+            if (prefixCategoryListItem == null)
+                return null;
+            if (prefixCategoryListItem.Name == null)
+                throw new ArgumentNullException(nameof(prefixCategoryListItem));
+
+            PrefixCategory prefixCategory = new PrefixCategory();
+            prefixCategory.Name = prefixCategoryListItem.Name;
+            prefixCategory.Description = prefixCategoryListItem.Description;
+            prefixCategory.Id = prefixCategoryListItem.Id;
+
+            return prefixCategory;
+        }
+    }
+}
