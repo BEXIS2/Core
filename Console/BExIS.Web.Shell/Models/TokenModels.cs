1c6ae97355b7c54311b7b8d4c38c724c97cba235
Sven Thiel <m6thsv2@googlemail.com>
2023-11-23 10:43:14 +0100

    #1276 add model for creation by set validation time frame

--- Console/BExIS.Web.Shell/Models/TokenModels.cs
+++ Console/BExIS.Web.Shell/Models/TokenModels.cs
@@ -5,6 +5,11 @@ using System.Web;
 
 namespace BExIS.Web.Shell.Models
 {
+    public class CreateJwtModel
+    {
+        public int Validity { get; set; }
+    }
+
     public class ReadJwtModel
     {
         public string Jwt { get; set; }
