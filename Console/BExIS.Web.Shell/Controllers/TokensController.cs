5c6217b01298782f82cd7579e609065a7d358636
Sven Thiel <m6thsv2@googlemail.com>
2023-11-23 10:42:42 +0100

    #1276 move tokens into a separate controller

--- Console/BExIS.Web.Shell/Controllers/TokensController.cs
+++ Console/BExIS.Web.Shell/Controllers/TokensController.cs
@@ -3,6 +3,7 @@ using BExIS.Security.Entities.Subjects;
 using BExIS.Security.Services.Subjects;
 using BExIS.Utils.Config;
 using BExIS.Web.Shell.Models;
+using Microsoft.AspNet.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,60 @@ namespace BExIS.Web.Shell.Controllers
 {
     public class TokensController : Controller
     {
+        public async Task<ActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateJwtModel model)
+        {
+            try
+            {
+                using (var identityUserService = new IdentityUserService())
+                using (var userManager = new UserManager())
+                {
+                    var jwtConfiguration = GeneralSettings.JwtConfiguration;
+
+                    long userId = 0;
+                    long.TryParse(this.User.Identity.GetUserId(), out userId);
+
+                    var user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration.IssuerSigningKey));
+                        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                        //Create a List of Claims, Keep claims name short
+                        var permClaims = new List<Claim>
+                        {
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                            new Claim(ClaimTypes.Name, user.UserName)
+                        };
+
+                        //Create Security Token object by giving required parameters
+                        var token = new JwtSecurityToken(jwtConfiguration.ValidIssuer,
+                        jwtConfiguration.ValidAudience,
+                        permClaims,
+                        notBefore: DateTime.Now,
+                            expires: model.Validity > 0 ? DateTime.Now.AddHours(model.Validity) : DateTime.Now.AddHours(jwtConfiguration.ValidLifetime),
+                            signingCredentials: credentials);
+
+                        var jwt_token = new JwtSecurityTokenHandler().WriteToken(token);
+
+                        return View("Read", model: new ReadJwtModel() { Jwt = jwt_token });
+                    }
+
+                    return View("Create");
+                }
+            }
+            catch
+            {
+                return View("Create");
+            }
+        }
+
         [HttpGet, JsonNetFilter]
         public async Task<JsonResult> GetToken()
         {
