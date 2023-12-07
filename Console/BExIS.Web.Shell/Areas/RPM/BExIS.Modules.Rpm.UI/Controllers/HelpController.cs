d7310bff7427302f5d2b998dd9879fb035bc0207
David Schöne <david.blaa@googlemail.com>
2023-12-07 16:01:10 +0100

    ##1467 update settings and add helper controller to rpm

---
+++ Console/BExIS.Web.Shell/Areas/RPM/BExIS.Modules.Rpm.UI/Controllers/HelpController.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Vaiona.Web.Mvc.Modularity;
+using Vaiona.Web.Mvc.Models;
+using Vaiona.Web.Extensions;
+using System.IO;
+using Vaiona.Utils.Cfg;
+using System.Xml;
+using BExIS.Xml.Helpers;
+using System.Xml.Linq;
+using System.Net;
+using System.Web;
+using System;
+using BExIS.Utils.Helpers;
+using BExIS.Utils.Config;
+
+namespace BExIS.Modules.Rpm.UI.Controllers
+{
+    public class HelpController : Controller
+    {
+        //
+        // GET: /ddm/Help/
+
+        public ActionResult Index()
+        {
+
+            string helpurl = ModuleManager.GetModuleSettings("Rpm").GetValueByKey("help").ToString();
+
+            //add default link if not set
+            if (String.IsNullOrEmpty(helpurl))
+            {
+                helpurl = ManualHelper.GetUrl(GeneralSettings.ApplicationVersion, "Rpm");
+            }
+           
+
+            return Redirect(helpurl);
+        }
+    }
+}
