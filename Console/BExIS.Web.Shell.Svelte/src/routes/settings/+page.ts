dc6f1be6f7520f4425f03b8c3a0ea2f0cacbd10c
Sven Thiel <m6thsv2@googlemail.com>
2023-12-19 09:24:19 +0100

    #965 use local environment

--- Console/BExIS.Web.Shell.Svelte/src/routes/settings/+page.ts
+++ Console/BExIS.Web.Shell.Svelte/src/routes/settings/+page.ts
@@ -6,7 +6,7 @@ export async function load() {
 
 	if (import.meta.env.DEV) {
 		// setApiConfig('https://dev.bexis2.uni-jena.de/', 'Admin', '123456');
-		setApiConfig('https://localhost:44345/', 'davidschoene', '123456');
+		setApiConfig('https://localhost:44345/', 'sventhiel', 'proq3dm6');
 
 	}
 
