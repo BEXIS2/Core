060b0802625e9528d463641860e14cb20c6cb863
Sven Thiel <m6thsv2@googlemail.com>
2023-12-20 15:16:43 +0100

    #1500 add correct party type inside settings to prevent issue with form after registration

---
+++ Console/BExIS.Web.Shell/Areas/BAM/Configurations/AccountPartyRelationshipType.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BExIS.Modules.Bam.UI.Configurations
+{
+    public class AccountPartyRelationshipType
+    {
+        public AccountPartyRelationshipType()
+        {
+            PartyRelationshipTypes = new List<string>();
+        }
+
+        [JsonProperty("partyRelationshipTypes")]
+        public List<string> PartyRelationshipTypes { get; set; }
+
+        [JsonProperty("partyType")]
+        public string PartyType { get; set; }
+    }
+}
---
+++ Console/BExIS.Web.Shell/Areas/BAM/Helpers/SettingsHelper.cs
@@ -0,0 +1,56 @@
+﻿using BExIS.Modules.Bam.UI.Configurations;
+using BExIS.Modules.Bam.UI.Models;
+using BExIS.Xml.Helpers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using Vaiona.Utils.Cfg;
+using Vaiona.Web.Mvc.Modularity;
+
+namespace BExIS.Modules.Bam.UI.Helpers
+{
+    public class SettingsHelper
+    {
+        private ModuleSettings _settings;
+        private string filePath = "";
+
+        public SettingsHelper()
+        {
+            _settings = ModuleManager.GetModuleSettings("bam");
+        }
+
+        public List<AccountPartyRelationshipType> GetAccountPartyRelationshipTypes()
+        {
+            try
+            {
+                return _settings.GetValueByKey<List<AccountPartyRelationshipType>>("AccountPartyRelationshipTypes");
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public string GetValue(string key)
+        {
+            XDocument settings = XDocument.Load(filePath);
+            XElement element = XmlUtility.GetXElementByAttribute("entry", "name", key.ToLower(), settings);
+
+            string value = "";
+            value = element.Attribute("value")?.Value;
+
+            return value;
+        }
+
+        public bool KeyExist(string key)
+        {
+            XDocument settings = XDocument.Load(filePath);
+            XElement element = XmlUtility.GetXElementByAttribute("entry", "name", key.ToLower(), settings);
+
+            return element != null ? true : false;
+        }
+    }
+}
