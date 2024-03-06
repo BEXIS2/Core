131f0cb6a4276ec4305ad0446df81dc45ebe2dfb
David Schöne <david.blaa@googlemail.com>
2024-03-06 14:38:23 +0100

    #1571 set display pattern id in variable

--- database update scripts/Update_Script_3beta_3.sql
+++ database update scripts/Update_Script_3beta_3.sql
@@ -15,6 +15,53 @@ SELECT label, description, 1, true,true
 FROM variables
 WHERE variablestype = 'VAR_TEMPL';
 
+-- list of display pattern
+-- xpath('Extra/DisplayPattern/Name:text)
+DO
+$do$
+DECLARE
+
+displaypattern text[] := ARRAY[
+'DateTimeIso',       
+'DateIso',           
+'DateUs',            
+'DateUs yy',         
+'DateUs M/d/yyyy',   
+'DateTimeUs h:m',    
+'DateUk',            
+'DateUk yy',         
+'DateEu',            
+'DateEu yy',         
+'Time',              
+'Time mm:ss',        
+'Time hh:mm',        
+'Time 12h',          
+'Time 12h hh:mm',    
+'yyyy-M-d',          
+'yyyy-d-M',          
+'yyyy-MM-dd',        
+'yyyy-dd-MM',        
+'d/M/yyyy hh:mm:ss tt',
+'Year',              
+'Month'          
+];
+
+dp_name text := 'DateUs';
+i int := 0;
+
+BEGIN
+
+i = array_position(displaypattern, dp_name);
+
+update variables as v
+set displaypatternid = array_position(displaypattern,(select unnest(xpath('Extra/DisplayPattern/Name/text()',extra))::text from datatypes where id = v.datatyperef))
+where v.datatyperef in (Select id from datatypes where systemtype = 'DateTime' and extra is not null);
+
+-- Select id, datatyperef, displaypatternid from variables where datatyperef in (3,4,13);
+
+END
+$do$
+
 
 
 
@@ -22,6 +69,9 @@ WHERE variablestype = 'VAR_TEMPL';
 INSERT INTO public.versions(
 	versionno, extra, module, value, date)
 	VALUES (1, null, 'Shell', '3.0.0',NOW());
+
+
+
 
 
 commit;
