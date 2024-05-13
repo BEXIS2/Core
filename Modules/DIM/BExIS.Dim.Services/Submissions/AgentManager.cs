eb9f3fc5ebab5e4f0569a2fed3648437fd6057fe
Sven Thiel <m6thsv2@googlemail.com>
2024-05-13 10:48:40 +0200

    #1567 update managers

---
+++ Modules/DIM/BExIS.Dim.Services/Submissions/AgentManager.cs
@@ -0,0 +1,84 @@
+﻿using BExIS.Dim.Entities.Submissions;
+using BExIS.Dlm.Entities.Party;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vaiona.Persistence.Api;
+
+namespace BExIS.Dim.Services.Submissions
+{
+    public class AgentManager : IDisposable
+    {
+        private readonly IUnitOfWork _guow;
+        private bool _isDisposed;
+
+        ~AgentManager()
+        {
+            Dispose(true);
+        }
+        public IReadOnlyRepository<Agent> AgentRepository { get; }
+        public IQueryable<Agent> Agents => AgentRepository.Query();
+        public AgentManager()
+        {
+            _guow = this.GetIsolatedUnitOfWork();
+            AgentRepository = _guow.GetReadOnlyRepository<Agent>();
+        }
+
+        public void Create(Agent agent)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var entityRequestRepository = uow.GetRepository<Agent>();
+                entityRequestRepository.Put(agent);
+                uow.Commit();
+            }
+        }
+
+        public void Delete(Agent agent)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var entityRequestRepository = uow.GetRepository<Agent>();
+                entityRequestRepository.Delete(agent);
+                uow.Commit();
+            }
+        }
+
+        public Agent FindById(long id)
+        {
+            return AgentRepository.Get(id);
+        }
+
+        public void Update(Agent entity)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var repo = uow.GetRepository<Agent>();
+                repo.Merge(entity);
+                var merged = repo.Get(entity.Id);
+                repo.Put(merged);
+                uow.Commit();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    if (_guow != null)
+                        _guow.Dispose();
+                    _isDisposed = true;
+                }
+            }
+        }
+    }
+}
---
+++ Modules/DIM/BExIS.Dim.Services/Submissions/SubmissionManager.cs
@@ -0,0 +1,79 @@
+﻿using BExIS.Dim.Entities.Submissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vaiona.Persistence.Api;
+
+namespace BExIS.Dim.Services.Submissions
+{
+    public class SubmissionManager : IDisposable
+    {
+        private readonly IUnitOfWork _guow;
+        private bool _isDisposed;
+
+        public IReadOnlyRepository<Submission> SubmissionRepository { get; }
+        public IQueryable<Submission> Submissions => SubmissionRepository.Query();
+        public SubmissionManager()
+        {
+            _guow = this.GetIsolatedUnitOfWork();
+            SubmissionRepository = _guow.GetReadOnlyRepository<Submission>();
+        }
+
+        public void Create(Submission submission)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var entityRequestRepository = uow.GetRepository<Submission>();
+                entityRequestRepository.Put(submission);
+                uow.Commit();
+            }
+        }
+
+        public Submission FindById(long id)
+        {
+            return SubmissionRepository.Get(id);
+        }
+
+        public void Update(Submission entity)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var repo = uow.GetRepository<Submission>();
+                repo.Merge(entity);
+                var merged = repo.Get(entity.Id);
+                repo.Put(merged);
+                uow.Commit();
+            }
+        }
+
+        public void Delete(Submission submission)
+        {
+            using (var uow = this.GetUnitOfWork())
+            {
+                var entityRequestRepository = uow.GetRepository<Submission>();
+                entityRequestRepository.Delete(submission);
+                uow.Commit();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    if (_guow != null)
+                        _guow.Dispose();
+                    _isDisposed = true;
+                }
+            }
+        }
+    }
+}
