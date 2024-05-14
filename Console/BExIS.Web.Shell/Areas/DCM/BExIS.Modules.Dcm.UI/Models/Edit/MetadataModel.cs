c6fb652744b12f8c5e40bd5b173ba80a9a62778f
David Schöne <david.blaa@googlemail.com>
2024-05-14 08:19:37 +0200

    #1715 add iframe component and settings to load external metadata from

---
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI/Models/Edit/MetadataModel.cs
@@ -0,0 +1,19 @@
+﻿using BExIS.IO.Transform.Validation.Exceptions;
+using BExIS.UI.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BExIS.Modules.Dcm.UI.Models.Edit
+{
+    public class MetadataModel
+    {
+        public bool UseExternalMetadataForm { get; set; }
+
+        public String ExternalMetadataFormUrl { get; set; }
+
+        public MetadataModel()
+        {
+            
+        }
+    }
+}
