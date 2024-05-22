425d97b21c6e4a77616c6986ffe4ce3bdc240809
Sven Thiel <m6thsv2@googlemail.com>
2024-05-22 19:38:04 +0200

    #1567 add manager for brokers

---
+++ Modules/DIM/BExIS.Dim.Services/Publications/BrokerManager.cs
@@ -0,0 +1,45 @@
+﻿using BExIS.Dim.Entities.Publications;
+using System;
+using System.Linq;
+using Vaiona.Persistence.Api;
+
+namespace BExIS.Dim.Services.Publications
+{
+    public class BrokerManager : IDisposable
+    {
+        private readonly IUnitOfWork _guow;
+        private bool _isDisposed;
+
+        public BrokerManager()
+        {
+            _guow = this.GetIsolatedUnitOfWork();
+            BrokerRepository = _guow.GetReadOnlyRepository<Broker>();
+        }
+
+        ~BrokerManager()
+        {
+            Dispose(true);
+        }
+
+        public IReadOnlyRepository<Broker> BrokerRepository { get; }
+        public IQueryable<Broker> Requests => BrokerRepository.Query();
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    if (_guow != null)
+                        _guow.Dispose();
+                    _isDisposed = true;
+                }
+            }
+        }
+    }
+}
