fd48e631553e39c1bb5b92a0fb3ca956c3948b9f
Sven Thiel <m6thsv2@googlemail.com>
2024-06-24 17:34:18 +0200

    #1567 update datacite concept

--- Console/BExIS.Web.Shell/Areas/DIM/Helpers/DimSeedDataGenerator.cs
+++ Console/BExIS.Web.Shell/Areas/DIM/Helpers/DimSeedDataGenerator.cs
@@ -518,6 +518,22 @@ namespace BExIS.Modules.Dim.UI.Helpers
 
                 if (!keys.Any(k => k.XPath.Equals("data/attributes/descriptions/descriptionType")))
                     conceptManager.CreateMappingKey("DescriptionType", "", "", false, false, "data/attributes/descriptions/descriptionType", concept, descriptions);
+
+                // dates
+                MappingKey dates = null;
+                if (!keys.Any(k => k.XPath.Equals("Dates")))
+                    dates = conceptManager.CreateMappingKey("Dates", "", "https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/date/#id1", true, true, "data/attributes/dates", concept);
+
+                if (!keys.Any(k => k.XPath.Equals("data/attributes/dates/date")))
+                    conceptManager.CreateMappingKey("Date", "", "", false, false, "data/attributes/descriptions/date", concept, dates);
+
+                if (!keys.Any(k => k.XPath.Equals("data/attributes/dates/dateType")))
+                    conceptManager.CreateMappingKey("Date", "", "", false, false, "data/attributes/descriptions/dateType", concept, dates);
+
+                if (!keys.Any(k => k.XPath.Equals("data/attributes/dates/dateInformation")))
+                    conceptManager.CreateMappingKey("DateInformation", "", "", true, false, "data/attributes/descriptions/dateInformation", concept, dates);
+            
+                
             }
         }
 
