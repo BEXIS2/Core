3efd0c954932c7d18fcbe854f904f5ae93149538
david schoene <david.schoene@uni-jena.de>
2024-11-04 15:04:49 +0100

    #1915 update cookie by every request, set jwt liftetime = cookie liftime

--- Components/App/BExIS.App.Bootstrap/Attributes/BExISApiAuthorizeAttribute.cs
+++ Components/App/BExIS.App.Bootstrap/Attributes/BExISApiAuthorizeAttribute.cs
@@ -1,13 +1,17 @@
-﻿using BExIS.Security.Entities.Subjects;
+﻿using BExIS.Security.Entities.Requests;
+using BExIS.Security.Entities.Subjects;
 using BExIS.Security.Services.Authorization;
 using BExIS.Security.Services.Objects;
 using BExIS.Security.Services.Subjects;
+using BExIS.Utils.Config;
 using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 
@@ -48,9 +52,24 @@ namespace BExIS.App.Bootstrap.Attributes
                         {
                             foreach (CookieState state in cookie.Cookies)
                             {
-                                if (state.Name.Equals("jwt")) jwt = state.Value;
+                                if (state.Name.Equals("jwt"))
+                                {
+                                    jwt = state.Value;
+                                }
                             }
                         }
+
+                  
+                        //actionContext.Request.
+
+                        //HttpCookie cookie = actionContext.Request.Cookies["YourCookieName"];
+
+                        //if (cookie != null)
+                        //{
+                        //    // Extend the expiration by 30 minutes from the current time
+                        //    cookie.Expires = DateTime.Now.AddMinutes(30);
+                        //    context.Response.Cookies.Add(cookie);
+                        //}
 
                         if (!string.IsNullOrEmpty(jwt))
                         {
@@ -116,6 +135,25 @@ namespace BExIS.App.Bootstrap.Attributes
                     }
 
                     actionContext.ControllerContext.RouteData.Values.Add("user", user);
+
+                    // update jwt cookie
+                    if (user != null)
+                    {
+                        var jwtConfiguration = GeneralSettings.JwtConfiguration;
+                        var jwt = JwtHelper.Get(user);
+              
+                        // Create a new cookie
+                        CookieHeaderValue cookie = new CookieHeaderValue("jwt", jwt);
+
+                        // Set additional properties if needed
+                        cookie.Expires = jwtConfiguration.ValidLifetime > 0 ? DateTime.Now.AddHours(jwtConfiguration.ValidLifetime) : DateTime.MaxValue;
+                        cookie.Domain = actionContext.Request.RequestUri.Host; // Set the domain
+                        cookie.Path = "/"; // Set the path
+
+                        // Add the cookie to the response
+                        actionContext.Response.Headers.AddCookies(new[] { cookie });
+                    }
+
                     return;
                 }
             }
@@ -123,5 +161,6 @@ namespace BExIS.App.Bootstrap.Attributes
             {
             }
         }
+
     }
 }
