c2593bdc1a18ca960f4dae66a6b6b4c358198a26
Sven Thiel <m6thsv2@googlemail.com>
2024-11-19 15:08:13 +0100

    #1111 minor updates

--- Modules/DIM/BExIS.Dim.Entities/Publications/Repository.cs
+++ Modules/DIM/BExIS.Dim.Entities/Publications/Repository.cs
@@ -1,10 +1,11 @@
-﻿using Vaiona.Entities.Common;
+﻿using System.Collections.Generic;
+using Vaiona.Entities.Common;
 
 namespace BExIS.Dim.Entities.Publications
 {
     public class Repository : BaseEntity, IBusinessVersionedEntity
     {
-        //public virtual Broker Broker { get; set; }
+        public virtual List<Broker> Broker { get; set; }
         public virtual string Name { get; set; }
 
         public virtual string Url { get; set; }
--- database update scripts/3.4.0-4.0.0.sql
+++ database update scripts/3.4.0-4.0.0.sql
@@ -2,7 +2,14 @@
     -- DefaultValue within MetadataAttributeUsage: varchar(255) -> text (check MetadataAttributeUsage.hbm.xml)
     -- FixedValue within MetadataAttributeUsage: varchar(255) -> text (check MetadataAttributeUsage.hbm.xml)
 
+-- DOI Mapping(s) Update
+INSERT INTO public.dim_mappingkeys (name, url, optional, iscomplex, concept, xpath)
+    SELECT 'Types', 'https://datacite-metadata-schema.readthedocs.io/en/4.5/properties/resourcetype/#a-resourcetypegeneral', false, true, (select id from dim_mappingconcepts where name='datacite'), 'data/attributes/types'
+    WHERE NOT EXISTS (select * from public.dim_mappingkeys where concept=(select id from dim_mappingconcepts where name='datacite') AND xpath='data/attributes/types');
 
+INSERT INTO public.dim_mappingkeys (name, optional, iscomplex, concept, parentref, xpath)
+    SELECT 'ResourceTypeGeneral', false, false, (select id from dim_mappingconcepts where name='datacite'), (select id from dim_mappingkeys where concept=(select id from dim_mappingconcepts where name='datacite') AND xpath='data/attributes/types'), 'data/attributes/types/resourceTypeGeneral'
+    WHERE NOT EXISTS (select * from public.dim_mappingkeys where concept=(select id from dim_mappingconcepts where name='datacite') AND xpath='data/attributes/types/resourceTypeGeneral');
 
 -- BEXIS2 Version Update
 INSERT INTO public.versions(
