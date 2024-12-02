e3b400436f207693db5e353293cd50ba41350118
Sven Thiel <m6thsv2@googlemail.com>
2024-12-02 11:20:48 +0100

    #1954 remove unnecessary code, unreachable parts, lib updates and update EmailService usage

--- Modules/DIM/BExIS.Dim.Helper/Export/PangaeaDataRepoConverter.cs
+++ Modules/DIM/BExIS.Dim.Helper/Export/PangaeaDataRepoConverter.cs
@@ -94,8 +94,6 @@ namespace BExIS.Dim.Helpers.Export
                 {
                     throw exception;
                 }
-
-                return "";
             }
             finally
             {
