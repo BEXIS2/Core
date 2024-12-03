3cfb8c92a77d4aff6813d84cfa8c8721571012cd
david schoene <david.schoene@uni-jena.de>
2024-12-03 16:56:38 +0100

    #1911 init dim.ui.svelte project

---
+++ Console/BExIS.Web.Shell/Areas/DIM/BExIS.Modules.Dim.UI.Svelte/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Page } from '@bexis2/bexis2-core-ui';
+
+</script>
+
+<Page title="System Administration">
+	<div class="grid grid-flow-col gap-10">
+		<a class="btn variant-filled-secondary" href="./gbif">gbif</a>
+
+	</div>
+</Page>
---
+++ Console/BExIS.Web.Shell/Areas/DIM/BExIS.Modules.Dim.UI.Svelte/src/routes/gbif/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+	import { Page, pageContentLayoutType} from "@bexis2/bexis2-core-ui";
+
+</script>
+<Page 
+	title="GBIF Export Manager"
+	note="Manage all datasets that are ready to exported to GBIF."
+	contentLayoutType={pageContentLayoutType.center}
+>
+
+
+</Page>
