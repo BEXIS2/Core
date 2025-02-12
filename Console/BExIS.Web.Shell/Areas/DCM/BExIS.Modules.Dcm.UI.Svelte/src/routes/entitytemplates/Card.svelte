830cec449d5ecf79afd9ab3ca43633fdc4449a52
david schoene <david.schoene@uni-jena.de>
2025-02-12 10:43:35 +0100

    #1973 change entity name and in use color

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/entitytemplates/Card.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/entitytemplates/Card.svelte
@@ -26,7 +26,7 @@
 			</div>
 			<div class="grow-0 w-42 text-right">
 				{#if linkedSubjects.length > 0}
-					<span class="badge variant-filled-error">in use</span>
+					<span class="badge variant-filled-success">in use</span>
 				{/if}
 				<span class="badge variant-filled-surface">{metadataStructure.text}</span>
 				<span class="badge variant-filled-secondary">{entityType.text}</span>
@@ -46,7 +46,7 @@
 		<!--linked subjects-->
 		{#if linkedSubjects.length > 0}
 			<i
-				>Show linked {entityType.text}:
+				>Show linked {entityType.text.toLowerCase()}:
 				<button class="btn-icon" on:click={() => (hidden = !hidden)}>
 					{#if hidden}
 						<Fa icon={faEye} />
