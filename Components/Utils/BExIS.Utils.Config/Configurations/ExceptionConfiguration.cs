811841184df364cfd3e5ef3838fa6d85f7b392cc
david schoene <david.schoene@uni-jena.de>
2025-06-30 15:22:56 +0200

    #2136 add exception settings

---
+++ Components/Utils/BExIS.Utils.Config/Configurations/ExceptionConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BExIS.Utils.Config.Configurations
+{
+    public class ExceptionConfiguration
+    {
+        [JsonProperty("sendExceptions")]
+        public bool sendExceptions { get; set; }
+
+        [JsonProperty("excludedHttpStatusCodes")]
+        public List<int> ExcludedHttpStatusCodes{ get; set; }
+
+        [JsonProperty("excludedMessages")]
+
+        public List<string> ExcludedMessages { get; set; }
+
+        public ExceptionConfiguration()
+        {
+            sendExceptions = false;
+            ExcludedHttpStatusCodes = new List<int>();
+            ExcludedMessages = new List<string>();
+        }
+    }
+}
