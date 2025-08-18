70766c632576339a0d604d48f07984f002a9b6eb
paul-mars <mars-paul@web.de>
2025-08-18 14:45:27 +0200

    #1992 fix start curation for longer greeting inputs and renamed introduction to greeting

--- Console/BExIS.Web.Shell/Areas/DDM/BExIS.Modules.Ddm.UI.Svelte/src/routes/curation/StartCuration.svelte
+++ Console/BExIS.Web.Shell/Areas/DDM/BExIS.Modules.Ddm.UI.Svelte/src/routes/curation/StartCuration.svelte
@@ -6,12 +6,12 @@
 
 	const { curation, editMode } = curationStore;
 
-	let introduction = '';
+	let greeting = '';
 	let tasks = '';
 
 	curationStore.addEmptyEntry(
 		{
-			name: introduction,
+			name: greeting,
 			description: tasks,
 			type: CurationEntryType.StatusEntryItem,
 			position: 0
@@ -21,18 +21,18 @@
 
 	const split = '\n\n---\n\n';
 
-	$: if (tasks.length === 0 && introduction.includes(split)) {
-		const splitIndex = introduction.indexOf(split);
-		const intro = introduction.slice(0, splitIndex);
-		const taskList = introduction.slice(splitIndex + split.length);
-		introduction = intro;
+	$: if (tasks.length === 0 && greeting.includes(split)) {
+		const splitIndex = greeting.indexOf(split);
+		const intro = greeting.slice(0, splitIndex);
+		const taskList = greeting.slice(splitIndex + split.length);
+		greeting = intro;
 		tasks = taskList;
 	}
 
 	const startCuration = () => {
 		if (!$curation?.curationStatusEntry?.isDraft() || !$curation?.isCurator) return;
 		curationStore.updateEntry($curation?.curationStatusEntry.id, {
-			name: introduction,
+			topic: greeting,
 			description: tasks
 		});
 		editMode.set(true);
@@ -53,12 +53,12 @@
 			Start Curation
 		</button>
 		<label class="block">
-			<span class="text-surface-700">Introduction:</span>
+			<span class="text-surface-700">Greeting:</span>
 			<textarea
-				bind:value={introduction}
+				bind:value={greeting}
 				class="textarea text-sm"
 				rows="6"
-				placeholder="Add an introduction to greet the researcher. (can still be adjusted later)"
+				placeholder="Add a greeting to greet the researcher. (can still be adjusted later)"
 				required
 			></textarea>
 		</label>
