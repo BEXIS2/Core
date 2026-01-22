5a3352b44093ad2a8aee3e436a09baede0b74db6
geofranzi <franzi82@gmx.de>
2026-01-22 13:31:00 +0100

    #https://github.com/BEXIS2/Core/issues/2295 Add controller and views for PUM

---
+++ Console/BExIS.Web.Shell/Areas/PUM/BExIS.Modules.PUM.UI/Controllers/ImportJSONController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vaiona.Web.Mvc;
+using Telerik.Web.Mvc;
+using Telerik.Web.Mvc.Extensions;
+using System;
+
+namespace BExIS.Modules.Pum.UI.Controllers
+{
+    public class ViewController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Console/BExIS.Web.Shell/Areas/PUM/BExIS.Modules.PUM.UI/Helpers/PUMSeedDataGenerator.cs
+++ Console/BExIS.Web.Shell/Areas/PUM/BExIS.Modules.PUM.UI/Helpers/PUMSeedDataGenerator.cs
@@ -17,10 +17,35 @@ namespace BExIS.Modules.Pum.UI.Helpers
     {
         public void GenerateSeedData()
         {
+
+
+            // Features
+            FeatureManager featureManager = new FeatureManager();
+            Feature PumModule =
+                    featureManager.FeatureRepository.Get().FirstOrDefault(f => f.Name.Equals("Publication Import and View Module"));
+                if (PumModule == null)
+                    PumModule = featureManager.Create("Publication Import and View Module", "Publication Import and View Module");
+
+
             // Operations
             using (OperationManager operationManager = new OperationManager())
             {
-                var homeController = operationManager.Find("PUM", "Home", "*") ?? operationManager.Create("PUM", "Home", "*");
+                var homeController = operationManager.Find("PUM", "Home", PumModule) ?? operationManager.Create("PUM", "Home", PumModule);
+            }
+
+            using (OperationManager operationManager = new OperationManager())
+            {
+                var importJSONController = operationManager.Find("PUM", "ImportJSON", PumModule) ?? operationManager.Create("PUM", "ImportJSON", PumModule);
+            }
+
+            using (OperationManager operationManager = new OperationManager())
+            {
+                var importCSVController = operationManager.Find("PUM", "ImportCSV", PumModule) ?? operationManager.Create("PUM", "ImportCSV", PumModule);
+            }
+
+            using (OperationManager operationManager = new OperationManager())
+            {
+                var viewController = operationManager.Find("PUM", "View", PumModule) ?? operationManager.Create("PUM", "View", PumModule);
             }
 
 
