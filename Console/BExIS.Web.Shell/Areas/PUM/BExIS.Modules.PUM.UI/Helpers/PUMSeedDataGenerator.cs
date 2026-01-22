8e844ba81198e885aaeb018d56817d0a5476e5ce
geofranzi <franzi82@gmx.de>
2026-01-22 09:51:38 +0100

    #https://github.com/BEXIS2/Core/issues/2295 Add PUM

---
+++ Console/BExIS.Web.Shell/Areas/PUM/BExIS.Modules.PUM.UI/Helpers/PUMSeedDataGenerator.cs
@@ -0,0 +1,34 @@
+﻿using BExIS.Dlm.Entities.Party;
+using BExIS.Dlm.Services.Party;
+using BExIS.Security.Services.Authorization;
+using BExIS.Security.Services.Objects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using Vaiona.Logging;
+using Vaiona.Utils.Cfg;
+
+namespace BExIS.Modules.Pum.UI.Helpers
+{
+    public class PUMSeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+            // Operations
+            using (OperationManager operationManager = new OperationManager())
+            {
+                var homeController = operationManager.Find("PUM", "Home", "*") ?? operationManager.Create("PUM", "Home", "*");
+            }
+
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
