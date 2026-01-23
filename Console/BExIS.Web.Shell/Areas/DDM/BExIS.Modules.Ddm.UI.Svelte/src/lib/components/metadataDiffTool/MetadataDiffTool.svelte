ab43e3f737cb362d8e7c694c377820d2a6e121f0
geofranzi <franzi82@gmx.de>
2026-01-23 21:54:54 +0100

    Data Curation Tools: Add metadata diff tool #2164

--- Console/BExIS.Web.Shell/Areas/DDM/BExIS.Modules.Ddm.UI.Svelte/src/lib/components/metadataDiffTool/MetadataDiffTool.svelte
+++ Console/BExIS.Web.Shell/Areas/DDM/BExIS.Modules.Ddm.UI.Svelte/src/lib/components/metadataDiffTool/MetadataDiffTool.svelte
@@ -1,26 +1,70 @@
 <script lang="ts">
-	import { Api, Spinner } from '@bexis2/bexis2-core-ui';
+	import { Api, Spinner, DropdownKVP, MultiSelect } from '@bexis2/bexis2-core-ui';
 	import { onMount } from 'svelte';
 	import DiffNode from './DiffNode.svelte';
+	import { SlideToggle } from '@skeletonlabs/skeleton';
 
-	export let datasetId: number | null = null;
-
+	export let datasetIdStart: number | null = null;
+	let datasetId: number | null = null;
 	// TODO: Fetch available datasets from API if needed
-	let datasets = datasetId ? [{ id: datasetId, name: 'Current Dataset' }] : [];
+	let datasets = []; // datasetId ? [{ id: datasetId, name: 'Current Dataset' }] : [];
 
-	async function fetchDataset() {
+	let datasetResponse1: { maxVersion?: number; error?: any } = {};
+	let datasetResponse2: { maxVersion?: number; error?: any } = {};
+
+	onMount(async () => {
+		// read id from URL if not provided (id is provided in route)
+		const urlSearchParams = new URLSearchParams(window.location.search);
+		const urlDatasetId = urlSearchParams.get('id');
+		if (urlDatasetId) {
+			datasetIdStart = parseInt(urlDatasetId);
+			console.log('Dataset ID from URL:', datasetIdStart);
+		}
+
+		datasets = datasets.length > 0 ? datasets : await fetchAllDatasets();
+		console.log(datasets);
+		if (datasetIdStart !== null) {
+			datasetId = datasetIdStart;
+			selectedDataset1 =
+				datasets.find((ds: { id: number; text: string }) => ds.id === datasetId) || null;
+			selectedDataset2 =
+				datasets.find((ds: { id: number; text: string }) => ds.id === datasetId) || null;
+		} else {
+			selectedDataset1 = datasets.length > 0 ? datasets[0] : null;
+			selectedDataset2 = datasets.length > 0 ? datasets[0] : null;
+		}
+
+		console.log('Selected Datasets:', selectedDataset1, selectedDataset2);
+		onChangeSelectedDataset1(new Event('init'));
+		onChangeSelectedDataset2(new Event('init'));
+	});
+
+	async function fetchAllDatasets() {
 		try {
-			let response = await Api.get('/api/dataset/' + datasetId);
+			let response = await Api.get('/api/dataset');
+
+			return response.data
+				.map((ds: any) => ({ id: ds.Id, text: ds.Id + ' ' + ds.Title }))
+				.sort((a: any, b: any) => b.id - a.id);
+		} catch (error) {
+			return { error };
+		}
+	}
+
+	async function fetchDataset(id: number) {
+		try {
+			let response = await Api.get('/api/dataset/' + id);
+			console.log('Fetched dataset info:', response.data);
 			return { maxVersion: response.data.version };
 		} catch (error) {
 			return { error };
 		}
 	}
 
-	async function fetchMetadataVersion(versionNumber: number) {
+	async function fetchMetadataVersion(versionNumber: number, id: number) {
 		try {
 			let response = await Api.get(
-				`/api/metadata/${datasetId}/version_number/${versionNumber}?simplifiedJson=1`
+				`/api/metadata/${id}/version_number/${versionNumber}?simplifiedJson=1`
 			);
 			return response.data;
 		} catch (error) {
@@ -28,111 +72,188 @@
 		}
 	}
 
-	let datasetResponse: { maxVersion?: number; error?: any } = {};
+	onMount(async () => {});
 
-	onMount(async () => {
-		datasetResponse = await fetchDataset();
-	});
+	let versions1: number[] = [];
+	let versions2: number[] = [];
 
-	$: versions = Array.from({ length: datasetResponse.maxVersion ?? 0 }, (_, i) => i + 1);
-
-	let selectedDataset1: { id: number; name: string } | null = null;
+	let selectedDataset1: { id: number; text: string } | null = null;
 	let selectedVersion1: number | null = null;
 	let metadata1: any = null;
 	let loading1 = false;
 
-	let selectedDataset2: { id: number; name: string } | null = null;
+	let selectedDataset2: { id: number; text: string } | null = null;
 	let selectedVersion2: number | null = null;
 	let metadata2: any = null;
 	let loading2 = false;
 
 	// TODO: Fetch datasets from API if multiple datasets are to be compared
-	$: selectedDataset1 =
-		datasets.length > 0 && selectedDataset1 === null ? datasets[0] : selectedDataset1;
-	$: selectedDataset2 = selectedDataset1;
 
-	$: selectedVersion1 =
-		versions.length > 0 && selectedVersion1 === null ? (versions.at(-2) ?? null) : selectedVersion1;
-	$: selectedVersion2 =
-		versions.length > 0 && selectedVersion2 === null ? (versions.at(-1) ?? null) : selectedVersion2;
+	// update datasetId when selection changes and refetch versions
+	function onChangeSelectedDataset1(event: Event) {
+		selectedVersion1 = null;
 
-	$: if (selectedVersion1 !== null) {
+		fetchDataset(selectedDataset1.id).then((response) => {
+			datasetResponse1 = response;
+			console.log('Dataset Response 1:', datasetResponse1);
+			versions1 = Array.from({ length: datasetResponse1.maxVersion ?? 0 }, (_, i) => i + 1);
+			selectedVersion1 = datasetResponse1.maxVersion ?? null;
+			onChangeSelectedVersion1(new Event('init'));
+		});
+	}
+
+	function onChangeSelectedDataset2(event: Event) {
+		selectedVersion2 = null;
+
+		fetchDataset(selectedDataset2.id).then((response) => {
+			datasetResponse2 = response;
+			versions2 = Array.from({ length: datasetResponse2.maxVersion ?? 0 }, (_, i) => i + 1);
+			selectedVersion2 = datasetResponse2.maxVersion ?? null;
+			onChangeSelectedVersion2(new Event('init'));
+		});
+	}
+
+	// $: selectedDataset2 = selectedDataset1;
+
+	$: selectedVersion1 = null;
+	$: selectedVersion2 = null;
+
+	function onChangeSelectedVersion1(event: Event) {
+		if (syncSelections && selectedDataset1 != selectedDataset2) {
+			selectedDataset2 = selectedDataset1;
+			onChangeSelectedDataset2(new Event('init'));
+		}
 		metadata1 = null;
 		loading1 = true;
-		fetchMetadataVersion(selectedVersion1).then((data) => {
+		if (selectedVersion1 === null) {
+			return;
+		}
+		console.log('Fetching metadata for version 1:', selectedVersion1);
+		fetchMetadataVersion(selectedVersion1, selectedDataset1.id).then((data) => {
 			metadata1 = data;
 			loading1 = false;
 		});
 	}
 
-	$: if (selectedVersion2 !== null) {
+	function onChangeSelectedVersion2(event: Event) {
+			if (syncSelections && selectedDataset1 != selectedDataset2) {
+			selectedDataset1 = selectedDataset2;
+			onChangeSelectedDataset1(new Event('init'));
+		}
 		metadata2 = null;
 		loading2 = true;
-		fetchMetadataVersion(selectedVersion2).then((data) => {
+		if (selectedVersion2 === null) {
+			return;
+		}
+		console.log('Fetching metadata for version 2:', selectedVersion2);
+		fetchMetadataVersion(selectedVersion2, selectedDataset2.id).then((data) => {
 			metadata2 = data;
 			loading2 = false;
 		});
 	}
+
+	let syncSelections: boolean = true;
 </script>
 
-{#if datasetResponse.error}
-	<p class="text-red-600">Error loading dataset: {datasetResponse.error.message}</p>
-{:else if datasetResponse.maxVersion !== undefined}
-	<div class="mx-4 mb-4 flex justify-around gap-x-8 gap-y-4">
-		<div class="flex flex-wrap gap-2">
-			<label>
-				<span>Dataset 1:</span>
-				<select
-					class="select min-w-40"
-					id="dataset1"
-					bind:value={selectedDataset1}
-					disabled={datasets.length <= 1}
-				>
-					{#each datasets as dataset}
-						<option value={dataset}>
-							{dataset.name}
-						</option>
-					{/each}
-				</select>
-			</label>
-			<label>
-				<span>Version 1:</span>
-				<select class="select min-w-40" id="version1" bind:value={selectedVersion1}>
-					{#each versions as version}
-						<option value={version}>
-							Version {version}{version === datasetResponse.maxVersion ? ' (Latest)' : ''}
-						</option>
-					{/each}
-				</select>
-			</label>
-		</div>
 
-		<div class="flex flex-wrap gap-2">
-			<label>
-				<span>Dataset 2:</span>
-				<select
-					class="select min-w-40"
+<h2 class="m-4 text-2xl font-bold">Metadata Diff Tool</h2>
+<p class="mx-4 mb-2 text-sm ">Select datasets and versions to compare their metadata.</p>
+
+{#if datasetResponse1.error || datasetResponse2.error}
+	<p class="text-red-600">Error loading dataset: {datasetResponse1.error.message}</p>
+{:else if datasetResponse1.maxVersion !== undefined}
+	<!-- toggle to sync selections -->
+<div class="mx-4 mb-2 text-sm ">
+		
+	<SlideToggle
+		active="bg-secondary-500"
+		size="sm"
+		id="to_index"
+		name={"syncSelections"}
+		bind:checked={syncSelections}
+		on:change={!syncSelections
+			? () => {}
+			: () => {
+					selectedDataset2 = selectedDataset1;
+					onChangeSelectedDataset2(new Event('init'));
+			  }}
+		>Sync selection</SlideToggle
+	>
+</div>
+	<div class="mx-4 mb-4 flex justify-around gap-x-8 gap-y-4">
+		<div class="flex flex-wrap gap-2 w-1/2">
+			<div class="w-full mb-2">
+				<MultiSelect
+					id="dataset1"
+					title="Select Dataset 1"
+					bind:source={datasets}
+					itemId="id"
+					itemLabel="text"
+					itemGroup="group"
+					complexSource={true}
+					complexTarget={true}
+					bind:target={selectedDataset1}
+					isMulti={false}
+					placeholder="-- Please select --"
+					clearable={false}
+					on:change={onChangeSelectedDataset1}
+				/>
+			</div>
+			<div class="w-full font-bold">{selectedDataset1 ? selectedDataset1.text : 'None'}</div>
+			<div class="w-full">
+				<label>
+					<span>Version 1:</span>
+					<select
+						class="select min-w-40"
+						id="version1"
+						bind:value={selectedVersion1}
+						on:change={onChangeSelectedVersion1}
+					>
+						{#each versions1 as version}
+							<option value={version}>
+								Version {version}{version === datasetResponse1.maxVersion ? ' (Latest)' : ''}
+							</option>
+						{/each}
+					</select>
+				</label>
+			</div>
+		</div>
+		<div class="flex flex-wrap gap-2 w-1/2">
+			<div class="w-full mb-2">
+				<MultiSelect
 					id="dataset2"
-					bind:value={selectedDataset2}
-					disabled={datasets.length <= 1}
-				>
-					{#each datasets as dataset}
-						<option value={dataset}>
-							{dataset.name}
-						</option>
-					{/each}
-				</select>
-			</label>
-			<label>
-				<span>Version 2:</span>
-				<select class="select min-w-40" id="version2" bind:value={selectedVersion2}>
-					{#each versions as version}
-						<option value={version}>
-							Version {version}{version === datasetResponse.maxVersion ? ' (Latest)' : ''}
-						</option>
-					{/each}
-				</select>
-			</label>
+					title="Select Dataset 2"
+					bind:source={datasets}
+					itemId="id"
+					itemLabel="text"
+					itemGroup="group"
+					complexSource={true}
+					complexTarget={true}
+					bind:target={selectedDataset2}
+					isMulti={false}
+					placeholder="-- Please select --"
+					clearable={false}
+					on:change={onChangeSelectedDataset2}
+				/>
+			</div>
+			<div class="w-full font-bold">{selectedDataset2 ? selectedDataset2.text : 'None'}</div>
+			<div class="w-full">
+				<label>
+					<span>Version 2:</span>
+					<select
+						class="select min-w-40"
+						id="version2"
+						bind:value={selectedVersion2}
+						on:change={onChangeSelectedVersion2}
+					>
+						{#each versions2 as version}
+							<option value={version}>
+								Version {version}{version === datasetResponse2.maxVersion ? ' (Latest)' : ''}
+							</option>
+						{/each}
+					</select>
+				</label>
+			</div>
 		</div>
 	</div>
 	{#if selectedVersion1 && selectedVersion2}
---
+++ Console/BExIS.Web.Shell/Areas/DDM/BExIS.Modules.Ddm.UI.Svelte/src/routes/metadiff/+page.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Curation from '$lib/components/curation/Curation.svelte';
+	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+	import Fa from 'svelte-fa';
+	import {
+		faAngleLeft,
+		faCircleDot,
+		faCodeCommit,
+		faCodeCompare,
+		faPlus
+	} from '@fortawesome/free-solid-svg-icons';
+	import { fade, slide } from 'svelte/transition';
+	import type { Readable } from 'svelte/store';
+	import { tick } from 'svelte';
+	import MetadataDiffTool from '$lib/components/metadataDiffTool/MetadataDiffTool.svelte';
+	import {
+		CurationEntryType,
+		type CurationEntryHelperModel,
+		type CurationEntryModel
+	} from '$lib/models/CurationEntry';
+
+	export let datasetId: number;
+
+datasetId = 0
+
+
+
+</script>
+
+<!-- How the component might be used with data next to it -->
+<div class="flex gap-x-12">
+	<div
+		class="max-h-fit-screen size-full overflow-y-auto rounded border border-surface-500 bg-surface-300"
+	>
+	
+
+	
+		
+				<MetadataDiffTool {datasetId} />
+		
+
+	</div>
+
+</div>
+
+<style lang="postcss">
+	.max-h-fit-screen {
+		max-height: calc(100vh - 12rem);
+	}
+</style>
