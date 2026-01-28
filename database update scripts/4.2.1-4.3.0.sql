9a023213d711edf80ea7ed757415203b2c3671c9
geofranzi <franzi82@gmx.de>
2026-01-28 23:46:28 +0100

    Add operation for PUM and metadiff #2295 #2164

---
+++ database update scripts/4.2.1-4.3.0.sql
@@ -0,0 +1,26 @@
+BEGIN TRANSACTION;
+
+-- BEXIS2 Version Update
+INSERT INTO public.versions(
+	versionno, extra, module, value, date)
+	VALUES (1, null, 'Shell', '4.3.0',NOW());
+
+INSERT INTO public.operations (versionno, extra, module, controller, action, featureref)
+SELECT 1, NULL, 'DDM', 'metadiff', '*', null 
+WHERE NOT EXISTS (SELECT * FROM public.operations WHERE module='DDM' AND controller='metadiff');
+
+INSERT INTO public.operations (versionno, extra, module, controller, action, featureref)
+SELECT 1, NULL, 'PUM', 'importcsv', '*', null 
+WHERE NOT EXISTS (SELECT * FROM public.operations WHERE module='PUM' AND controller='importcsv');
+
+INSERT INTO public.operations (versionno, extra, module, controller, action, featureref)
+SELECT 1, NULL, 'PUM', 'importjson', '*', null 
+WHERE NOT EXISTS (SELECT * FROM public.operations WHERE module='PUM' AND controller='importjson');
+
+INSERT INTO public.operations (versionno, extra, module, controller, action, featureref)
+SELECT 1, NULL, 'PUM', 'view', '*', null 
+WHERE NOT EXISTS (SELECT * FROM public.operations WHERE module='PUM' AND controller='view');
+
+
+
+commit;
