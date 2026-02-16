eea0a5b483022e4743077956bc5721776750651f
geofranzi <franzi82@gmx.de>
2026-02-16 10:28:41 +0100

    Correct link on index page to new name for componentconfig #2361

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/+page.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/+page.svelte
@@ -18,7 +18,7 @@
 		<a class="btn variant-filled-secondary" href="./create">create</a>
 		<a class="btn variant-filled-secondary" href="./edit">edit</a>
 		<a class="btn variant-filled-secondary" href="./view">view</a>
-		<a class="btn variant-filled-secondary" href="./componentconfiguration">Component Configuration</a>
+		<a class="btn variant-filled-secondary" href="./componentconfig">Component Configuration</a>
 		<a class="btn variant-filled-secondary" href="./metadata">metadata</a>
 	</div>
 </Page>
