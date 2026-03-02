72e180eee79c3dd4baa78f900db878e9d73d8959
david schoene <david.schoene@uni-jena.de>
2026-03-02 14:01:09 +0100

    #2385 seperate validation and button functions into metadata function

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/metadata/edit/+page.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/metadata/edit/+page.svelte
@@ -3,46 +3,25 @@
 
 
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import ComplexComponent from './complexComponentWrapper.svelte';
 
 	import * as apiCalls from '../services/apiCalls';
 	import { helpStore, notificationType, Page, pageContentLayoutType, Spinner } from '@bexis2/bexis2-core-ui';
 	import Functions from './MetadataFunctions.svelte';
-	import suite from './simpleComponent';
 
 	// import { Page } from '@bexis2/bexis2-core-ui';
 	import { schemaToJson, setConfigStore, setMetadataStore } from '$lib/components/utils/metadata/metadataComponentUtils';
-	import { type	notificationItemType, notificationStore } from '@bexis2/bexis2-core-ui';
-	import { validationStore } from '$lib/components/utils/metadata/stores';
-	import type { validationStoretype } from '$lib/components/utils/metadata/models';
-	import { get } from 'svelte/store';
-
-
-// This regex accepts HH:mm:ss without requiring Z
 
 
 	// import configJson from './customComponents/config.json';
 	//export let schemaId: number = 3;
 	export let datasetId: number = 2;
-	export let saveWithError: boolean = true;
-	let disbaleSaveBtn: boolean = false;
-	$:disbaleSaveBtn;
-
-	let errors:any[]	= [];
+	export let saveWithError: boolean = false;
 
 	let s: any;
 	let m: any = null;
 	let schema: any = s;
 	$: schema = s;
-
-	let validationStoreValues: validationStoretype;
-
-$:{
- validationStoreValues;
-	disbaleSaveBtn = disableSaveFn();
-	console.log("🚀 ~ file: +page.svelte:92 ~ $: ~ disbaleSaveBtn:", disbaleSaveBtn)
-}
 
 
 	async function load() {
@@ -61,100 +40,23 @@ $:{
 			const configJson = await apiCalls.GetComponentConfig(datasetInfos.entityTemplateId, "edit");
 			setConfigStore(configJson);
 
-			
-			// listen to  validation store changes
-			validationStoreValues	= get(validationStore);
-
-			validationStore.subscribe((s) => {
-     validationStoreValues = s;
-    });
-
 		}
 	}
 
- let valid: boolean = true;
-	$:valid;
-
-
-	function validateFn() {
-		
-		if (!validationStoreValues.allSimpleRequiredValid) 
-		{
-			//  console.log(validationStoreValues.simpleTypeValidationItems.filter(item => item.isValid === false));
-			// // errors = validate.errors;
-
-			notificationStore.showNotification({
-				notificationType: notificationType.error,
-				message: 'error validating metadata. Please check the console for details.',
-			});
-
-		}
-		else {
-			errors = [];
-			notificationStore.showNotification({
-				notificationType: notificationType.success,
-				message: 'Metadata is valid.',
-			});
-		}
-	}
-
-
-	function disableSaveFn():boolean {
-
-		console.log("🚀 ~ disbaleSaveFn ~ validationResult:")
-
-		if (saveWithError) return false; // when save with error is allowd, the save button is always enabled
-		if (!validationStoreValues) return true; // if there is no validation result, we consider the form as not valid, so the save button is disabled
-		console.log("🚀 ~ disableSaveFn ~ validationStoreValues:", validationStoreValues)
-	
-		return !validationStoreValues.allSimpleRequiredValid; //	disable save button when the metadata is not valid
-
-	}
 
 </script>
 
 
 
-<Page contentLayoutType={pageContentLayoutType.full}  footer={false} >
+<Page contentLayoutType={pageContentLayoutType.full}  footer={true} >
 	{#await load()}
 		<Spinner />
 	{:then}
 
 <div	class="container">
 	<div class="nav-left scrollable">
-
-		<div id="metadata-options" >
-			<button class="btn variant-filled-secondary m-2" on:click={validateFn}>
-				validate
-			</button>
-
-			<button
-				class="btn variant-filled-secondary m-2"
-				disabled={disbaleSaveBtn}
-
-				on:click={async () => {
-					try {
-						console.log('Saving metadata:', datasetId, m);
-						const savedMetadata = await apiCalls.SaveMetadata(datasetId, m);
-						console.log('Metadata saved successfully:', savedMetadata);
-						notificationStore.showNotification({
-							notificationType: notificationType.success,
-							message: 'Metadata saved successfully.',
-						});
-					} catch (error) {
-						console.error('Error saving metadata:', error);
-						notificationStore.showNotification({
-							notificationType: notificationType.error,
-							message: 'Error saving metadata. Please check the console for details.',
-						});
-					}
-				}}>
-				Save Metadata
-			</button>
-		</div>
-
 		{#if m}
-			<Functions bind:data={m}/>
+			<Functions bind:metadata={m} saveWithError={saveWithError} bind:datasetId={datasetId} />
 		{/if}
 	</div>
 
--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/metadata/edit/MetadataFunctions.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/metadata/edit/MetadataFunctions.svelte
@@ -1,82 +1,158 @@
 
 <script lang="ts">
 
-
-	 import Fa from 'svelte-fa';
+	import Fa from 'svelte-fa';
   import { faCheck } from '@fortawesome/free-solid-svg-icons';
 	import { onMount } from 'svelte';
+  import * as apiCalls from '../services/apiCalls';
 	import { getValidationStore } from '$lib/components/utils/metadata/metadataComponentUtils';
 	import type { validationStoretype } from '$lib/components/utils/metadata/models';
- import {validationStore} from '$lib/components/utils/metadata/stores';
-	import { get } from 'svelte/store';
-	import { each } from 'vest';
+  import {metadataStore, validationStore} from '$lib/components/utils/metadata/stores';
 
-  export let data;
-  
-  // Check if the current data is something we can expand
-  const isObject = data !== null && typeof data === 'object';
-  let expanded = false;
+
+	import { notificationStore, notificationType } from '@bexis2/bexis2-core-ui';
+
+  export let datasetId: number;
+  export let metadata;
+  export let saveWithError: boolean = false;
+  let hasChanged: boolean = true; // need to implement change detection to enable/disable save button based on whether there are unsaved changes or not, for now it's always enabled
+
+  const unsubscribedMetadata = metadata;
+
+
+  $:metadata, console.log("functions - metadata:", metadata);
+
+	let disbaleSaveBtn: boolean = false;
+	$:disbaleSaveBtn;
 
 	 let validationStoreValues: validationStoretype;
-
-
-  function hasErrors(key) {
-
-   if(validationStoreValues){
-
-    const invalidParts = validationStoreValues.simpleTypeValidationItems.filter(item => item.path.startsWith(key) && item.isValid === false);
-    
-
-     return (invalidParts && invalidParts.length > 0);
-   };
+  $:{
+   validationStoreValues;
+    disbaleSaveBtn = disableSaveFn();
+    console.log("🚀 ~ file: +page.svelte:92 ~ $: ~ disbaleSaveBtn:", disbaleSaveBtn)
   }
 
   onMount(() => {
-    console.log("🚀 ~ Tree Component - Data:", data);
-    
+  
+    metadataStore.subscribe((s) => {
+      metadata = s;
+    });
+
     validationStore.subscribe((s) => {
      validationStoreValues = s;
-     console.log("🚀 ~ Tree validationStoreValues:", validationStoreValues)
     });
   
   });
 
+  
+  function hasErrors(key) {
+
+   if(validationStoreValues){
+    const invalidParts = validationStoreValues.simpleTypeValidationItems.filter(item => item.path.startsWith(key) && item.isValid === false);
+     return (invalidParts && invalidParts.length > 0);
+   };
+  }
+
+	function validateFn() {
+		
+		if (!validationStoreValues.allSimpleRequiredValid) 
+		{
+
+			notificationStore.showNotification({
+				notificationType: notificationType.error,
+				message: 'error validating metadata. Please check the console for details.',
+			});
+
+		}
+		else {
+			notificationStore.showNotification({
+				notificationType: notificationType.success,
+				message: 'Metadata is valid.',
+			});
+		}
+	}
+
+	function disableSaveFn():boolean {
+    if (hasChanged == false) return true; // when there are changes, the save button is enabled, so return false for disabled
+		if (saveWithError) return false; // when save with error is allowd, the save button is always enabled
+		if (!validationStoreValues) return true; // if there is no validation result, we consider the form as not valid, so the save button is disabled
+	
+		return !validationStoreValues.allSimpleRequiredValid; //	disable save button when the metadata is not valid
+	}
 </script>
-<nav class="list-nav">
-{#if validationStoreValues}
+<div class="flex flex-col gap-2" >
+<div id="metadata-options" class="flex w-full" >
+    <div class="flex-auto"> 
+          <button class="btn variant-filled-secondary m-2" on:click={validateFn}>
+            validate
+          </button>
+    </div>
+			<button
+				class="btn variant-filled-primary m-2"
+				disabled={disbaleSaveBtn}
 
+				on:click={async () => {
+					try {
 
-{#key validationStoreValues}
- 
- {#if validationStoreValues.allSimpleRequiredValid}
-  <span class="text-success-500"><Fa icon={faCheck} /></span>
-  {:else}
-  <span class="text-error-500">#</span>
-  {#each validationStoreValues.simpleTypeValidationItems.filter(item => item.isValid === false) as item}
-   <div class="text-error-500 ml-4">
-    {item.path}: {item.errorMessage}
-   </div>
-  {/each}
+						console.log('Saving metadata:', datasetId, metadata);
+						const savedMetadata = await apiCalls.SaveMetadata(datasetId, metadata);
+						console.log('Metadata saved successfully:', savedMetadata);
+						notificationStore.showNotification({
+							notificationType: notificationType.success,
+							message: 'Metadata saved successfully.',
+						});
+					} catch (error) {
+						console.error('Error saving metadata:', error);
+						notificationStore.showNotification({
+							notificationType: notificationType.error,
+							message: 'Error saving metadata. Please check the console for details.',
+						});
+					}
+				}}>
+				Save Metadata
+			</button>
+</div>
+<!-- Error messages-->
+ <div >
+ {#if validationStoreValues}
+  {#key validationStoreValues}
+  
+  {#if !validationStoreValues.allSimpleRequiredValid}
+  <hr/>
+    <span class="text-error-500">#</span>
+    {#each validationStoreValues.simpleTypeValidationItems.filter(item => item.isValid === false) as item}
+    <div class="text-error-500 ml-4">
+      {item.path}: {item.errorMessage}
+    </div>
+    {/each}
   {/if}
-
-
-
- <ul>
-  {#each Object.entries(data) as [key, value]}
-  {#if typeof value === 'object' && value !== null}
-    <a href="#{key}" >
-     <span class="flex-auto">{key}</span>
-     {#if validationStoreValues && hasErrors(key)}
-      <span class="text-error-500">#</span>
-     {:else if validationStoreValues && !hasErrors(key)}
-      <span class="text-success-500"><Fa icon={faCheck} /></span>
-     {/if}
-			</a>
+  {/key}
   {/if}
-  {/each}
- </ul>
-{/key}
-{/if}
+</div>
 
- </nav>
+<!-- Main blocks-->
+<div>
+  <hr/>
+  <nav class="list-nav">
+  {#if validationStoreValues}
+  {#key validationStoreValues}
+  <ul>
+    {#each Object.entries(metadata) as [key, value]}
+    {#if typeof value === 'object' && value !== null}
+      <a href="#{key}" >
+      <span class="flex-auto">{key}</span>
+      {#if validationStoreValues && hasErrors(key)}
+        <span class="text-error-500">#</span>
+      {:else if validationStoreValues && !hasErrors(key)}
+        <span class="text-success-500"><Fa icon={faCheck} /></span>
+      {/if}
+        </a>
+    {/if}
+    {/each}
+  </ul>
+  {/key}
+  {/if}
+  </nav>
+ </div>
+ </div>
 
