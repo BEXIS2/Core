d6cebc73c9c8fb7d1875dfa1b69fe3ed7947e630
geofranzi <franzi82@gmx.de>
2026-07-09 14:31:52 +0200

    #2527 Change data structure validation messages

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/lib/components/validation/Message.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/lib/components/validation/Message.svelte
@@ -9,7 +9,7 @@
 
 <Accordion>
 	<AccordionItem>
-		<svelte:fragment slot="summary"><div class="font-bold justify-self-end">{count}&nbsp;{type === 'error' ? 'Errors' : 'Warnings'}</div></svelte:fragment>
+		<svelte:fragment slot="summary"><div class="font-bold justify-self-end">{count}&nbsp;{count === 1 ? (type === 'error' ? 'Error' : 'Warning') : (type === 'error' ? 'Errors' : 'Warnings')}</div></svelte:fragment>
 		<svelte:fragment slot="lead"><div class="break-words">{title}</div></svelte:fragment>
 		<svelte:fragment slot="content">
 			<ol class="list-disc px-5 break-words w-11/12">
