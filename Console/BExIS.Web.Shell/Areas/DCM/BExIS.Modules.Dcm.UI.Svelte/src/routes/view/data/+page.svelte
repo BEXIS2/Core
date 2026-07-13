820b453c600d47797d8fc70c4df909748551b486
david schoene <david.schoene@uni-jena.de>
2026-07-13 09:14:55 +0200

    #907 add fileview to data

---
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/routes/view/data/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+
+	import { Page, pageContentLayoutType } from "@bexis2/bexis2-core-ui";
+	import PrimaryData from "$lib/components/data/PrimaryData.svelte";
+	import { onMount } from "svelte";
+	import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+	import Fa from "svelte-fa";
+	import Back from "$lib/components/utils/Back.svelte";
+ 
+
+
+export let id = 0;
+export let version = 1;
+export let	versionId	= -1;
+
+	onMount(async () => {
+
+	 let container;
+  container = document.getElementById('data');
+		id = container?.getAttribute('dataset');
+		version = container?.getAttribute('version');
+		versionId = container?.getAttribute('versionId');
+
+
+	});
+
+
+</script>
+
+<Page title="Data of entity ({id})" contentLayoutType={pageContentLayoutType.full}>
+
+	<Back	/>
+ 
+	{#if id	> 0}
+		<PrimaryData {id} {version} />
+	{:else}
+		<p class="text-center text-lg">No data available</p>
+	{/if}
+
+</Page>
