62fea9d9cce98cd404d9713ceb9644789545c8b8
geofranzi <franzi82@gmx.de>
2026-07-30 15:37:33 +0200

    #2431 Animate placeholder

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/lib/hooks/edit/placeholder/PlaceHolderHookContent.svelte
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/lib/hooks/edit/placeholder/PlaceHolderHookContent.svelte
@@ -1,22 +1,22 @@
 <div class="grow space-y-2">
 	<div class="h-full w-full">
 		<div class="space-y-4">
-			<div class="placeholder" />
+			<div class="placeholder-block h-6 w-full" />
 			<div class="grid grid-cols-3 gap-8">
-				<div class="placeholder" />
-				<div class="placeholder" />
-				<div class="placeholder" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
 			</div>
 			<div class="grid grid-cols-4 gap-4">
-				<div class="placeholder" />
-				<div class="placeholder" />
-				<div class="placeholder" />
-				<div class="placeholder" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
 			</div>
 			<div class="grid grid-cols-3 gap-4">
-				<div class="placeholder" />
-				<div class="placeholder" />
-				<div class="placeholder" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
+				<div class="placeholder-block h-6" />
 			</div>
 		</div>
 	</div>
