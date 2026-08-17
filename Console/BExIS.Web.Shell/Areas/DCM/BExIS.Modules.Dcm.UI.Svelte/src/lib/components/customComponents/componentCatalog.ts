21795ba538b651e9db2dc4e50a9a21a087be0389
geofranzi <franzi82@gmx.de>
2026-08-17 22:28:42 +0200

    #2543 #235 #2542 Add Text area, ror and orcid to component catalogue

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/lib/components/customComponents/componentCatalog.ts
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI.Svelte/src/lib/components/customComponents/componentCatalog.ts
@@ -1,8 +1,11 @@
 import dateRangepicker from './DateRangePicker_0.1/component.svelte';
 import terminologyService_1_0_0 from './TerminologyService_1_0_0/component.svelte';
 import TextField_1_0_0 from './TextField_1_0_0/component.svelte';
+import TextArea_1_0_0 from './TextArea_1_0_0/component.svelte';
 import HorizontalAlignment_1_0_0 from './HorizontalAlignment_1_0_0/component.svelte';
 import DefaultValues_1_0_0 from './DefaultValues_1_0_0/component.svelte';
+import Ror_1_0_0 from './Ror_1_0_0/component.svelte';
+import Orcid_1_0_0 from './Orcid_1_0_0/component.svelte';
 
 
 export const customComponentsCatalog: any = {
@@ -12,8 +15,14 @@ export const customComponentsCatalog: any = {
         component: terminologyService_1_0_0},  
     'textField_v1.0.0': {
         component: TextField_1_0_0},
+    'textArea_v1.0.0': {
+        component: TextArea_1_0_0},
     'horizontalAlignment_v1.0.0': {
         component: HorizontalAlignment_1_0_0},
     'defaultValues_v1.0.0': {
-        component: DefaultValues_1_0_0 }
+        component: DefaultValues_1_0_0 },
+    'ror_v1.0.0': {
+        component: Ror_1_0_0 },
+    'orcid_v1.0.0': {
+        component: Orcid_1_0_0 }
 };
