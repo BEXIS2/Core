f931072c4988c7f62a82d3773472330f48b3e103
geofranzi <franzi82@gmx.de>
2026-08-24 23:49:30 +0200

    #2545 fix GetCitationOptions do not handle 0 versions or tag

--- Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI/Controllers/ViewController.cs
+++ Console/BExIS.Web.Shell/Areas/DCM/BExIS.Modules.Dcm.UI/Controllers/ViewController.cs
@@ -518,9 +518,24 @@ namespace BExIS.Modules.Dcm.UI.Controllers
 
             using (var datasetManager = new DatasetManager())
             {
-                var datasetVersion = datasetManager.GetDatasetVersion(id, version);
+                DatasetVersion datasetVersion;
 
-                if (datasetVersion == null) throw new ArgumentException("Version noi");
+                if (version <= 0 && tag <= 0)
+                {
+                    // no version or tag specified — use latest version
+                    datasetVersion = datasetManager.GetDatasetLatestVersion(id);
+                }
+                else if (tag > 0)
+                {
+                    // tag specified — get latest version by tag
+                    datasetVersion = datasetManager.GetLatestDatasetVersionByTag(id, tag, false);
+                }
+                else
+                {
+                    datasetVersion = datasetManager.GetDatasetVersion(id, version);
+                }
+
+                if (datasetVersion == null) throw new ArgumentException("Version not found");
 
                 long datastructureId = datasetVersion.Dataset.DataStructure != null ? datasetVersion.Dataset.DataStructure.Id : -1;
                 var moduleSettings = ModuleManager.GetModuleSettings("Ddm");
